3395101616651c10890672fd1a45c3aab282e35b
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2008-12-05 16:35:22 +0000

    Prepare an intermediate release 0.65 from the branch 0.70. Changed the version string.

--- src/jsxgraph.js
+++ src/jsxgraph.js
@@ -38,7 +38,7 @@
  * @param {String} forceRenderer If a specific renderer should be chosen. Possible values are 'vml', 'svg', 'silverlight'
  */
 JXG.JSXGraph = new function (forceRenderer) {
-    this.licenseText = 'JSXGraph v0.70 Copyright (C) 2008 see http://jsxgraph.org';
+    this.licenseText = 'JSXGraph v0.65 Copyright (C) 2008 see http://jsxgraph.org';
 
     /**
      * Stores the renderer that is used to draw the board.
