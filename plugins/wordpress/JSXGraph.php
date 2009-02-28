332be087da7e79f1eef429239fd0fe7c3cf26eae
Peter Wilfahrt <peter@techi.de>
2009-02-28 18:34:18 +0000

    should work except the wordpress autoformat

--- plugins/wordpress/JSXGraph.php
+++ plugins/wordpress/JSXGraph.php
@@ -1,8 +1,15 @@
 <?php
+/*
+Plugin Name: JSXGraph
+Plugin URI: http://jsxgraph.org
+Description: Embeding jsxgraph constructions
+Author: Peter Wilfahrt
+Version: 0.1
+Author URI: http://www.webconsul.de/
+*/
+
 /* TODO:
- * working method for default boards, especially if there's > 1
- * return text around tags after parsing (the text before and after)
- * get around wordpress content char filter
+ * fix wordpress auto format
  * make downloadable package with styles/scripts
  * publish on wordpress.org
  * create an insert button with all parameters
@@ -38,75 +45,69 @@ function jsxgraph_head() {
 }
 
 function jsxgraph_filter($text) {
-	// to optimize speed, search for a jsxgraph-tag (avoiding to parse everything on every text)
-	if(is_int(strpos($text, '<jsxgraph'))) {
-		$jsxgraph_version = '0.1';
-		$output  = '<!-- JSXGraph WordPress extension '. $jsxgraph_version .' -->';
+  if(is_int(strpos($text, '<jsxgraph'))) {
+    $jsxgraph_version = '0.1';
 
-		$jxg = array();
-		// get every construction
-		for($i = 0; $i < substr_count($text, '<jsxgraph'); $i++) {
-			$start   = ($end) ? strpos($text, '<jsxgraph', $end) : strpos($text, '<jsxgraph');
-			$start += 10;
-			$end     = (is_int(strpos($text, '</jsxgraph>', $start))) ? strpos($text, '</jsxgraph>', $start) : strpos($text, '/>', $start);
-			$jxg[$i] = substr($text, $start, $end-$start);
-		}
+    // get every construction
+    $count = substr_count($text, '<jsxgraph');
+    for($i = 0; $i < $count; $i++) {
+      $start = strpos($text, '<jsxgraph');
+      $end = (is_int(strpos($text, '</jsxgraph>', $start))) ? strpos($text, '</jsxgraph>', $start)+11 : strpos($text, '/>', $start)+2;
+      $jxg = substr($text, $start+10, $end-$start-21);
 
-		// parse parameters of construction
-		for($i = 0; $i < sizeof($jxg); $i++) {
-			$input = split(">", $jxg[$i]); // fix for javascript construction input
-			$input[0] = str_replace("'", '', $input[0]);
-			$input[0] = str_replace('"', '', $input[0]);
-			$input[0] = str_replace(' ', '&', $input[0]);
-			parse_str($input[0], $params);
+      // parse parameters of construction
+      $input = split(">", $jxg); // fix for javascript construction input
+      $input[0] = str_replace("'", '', $input[0]);
+      $input[0] = str_replace('"', '', $input[0]);
+      $input[0] = str_replace(' ', '&', $input[0]);
+      parse_str($input[0], $params);
 
-			// defaults need to be changed if there's more than one box
-			$outputDivId   = (isset($params['box']))      ? htmlspecialchars(strip_tags($params['box']))      : 'box'.$i;
-			// defaults need to be changed if there's more than one board
-			$outputBoardId = (isset($params['board']))    ? htmlspecialchars(strip_tags($params['board']))    : 'board'.$i;
-			$width         = (isset($params['width']))    ? htmlspecialchars(strip_tags($params['width']))    : 500;
-			$height        = (isset($params['height']))   ? htmlspecialchars(strip_tags($params['height']))   : 400;
+      $outputDivId   = (isset($params['box']))    ? htmlspecialchars(strip_tags($params['box']))    : 'box'.$i;
+      $outputBoardId = (isset($params['board']))  ? htmlspecialchars(strip_tags($params['board']))  : 'board'.$i;
+      $width         = (isset($params['width']))  ? htmlspecialchars(strip_tags($params['width']))  : 500;
+      $height        = (isset($params['height'])) ? htmlspecialchars(strip_tags($params['height'])) : 400;
 
-			// Output div
-			$output .= "<div id='". $outputDivId ."' class='jxgbox' style='width:". $width ."px; height:". $height ."px;'></div>";
-			$output .= "<script type='text/javascript'>";
+      // output div
+      $output  = "<div id='". $outputDivId ."' class='jxgbox' style='width:". $width ."px; height:". $height ."px;'></div>";
+      $output .= "<script type='text/javascript'>";
 
-			// construction by filename
-			if(isset($params['filename'])) {
-				$gxtBinary = htmlspecialchars(strip_tags($params['filename']));
-				$gxtFile = Image::newFromName($gxtBinary);
-				if (!($gxtFile->exists() )) {
-					$error_message = "File " . $gxtFile . " not found.";
-				} else {
-					$gxtURL = $CFG->wwwroot . $gxtFile->getURL();
-				}
-				$output .= "  var ". $outputBoardId ." = JXG.JSXGraph.loadBoardFromFile('". $outputDivId."', '". $gxtURL ."', 'Geonext');";
+      // construction by filename
+      if(isset($params['filename'])) {
+        $gxtBinary = htmlspecialchars(strip_tags($params['filename']));
+        $gxtFile = Image::newFromName($gxtBinary);
+        if (!($gxtFile->exists() )) {
+          $error_message = "File " . $gxtFile . " not found.";
+        } else {
+          $gxtURL = get_bloginfo('wpurl') . $gxtFile->getURL();
+        }
+        $output .= "  var ". $outputBoardId ." = JXG.JSXGraph.loadBoardFromFile('". $outputDivId ."', '". $gxtURL ."', 'Geonext');";
+      }
+      // construction by filestring
+      else if(isset($params['filestring'])) {
+        $tmp = split("filestring=", $text);
+        $tmp[1] = str_replace("'", '"', $tmp[1]);
+        $tmp = split('"', $tmp[1]);
+        $filestring = htmlspecialchars(strip_tags($tmp[1]));
+        $output .= "  var ". $outputBoardId ." = JXG.JSXGraph.loadBoardFromString('". $outputDivId ."', '". $filestring ."', 'Geonext');";
+      }
+      // construction by $input
+      else {
+        $output .= $input[1];
+      }
+      $output .= "</script>";
 
-			}
-			// construction by filestring
-			else if(isset($params['filestring'])) {
-				$tmp = split("filestring=", $text);
-				$tmp[1] = str_replace("'", '"', $tmp[1]);
-				$tmp = split('"', $tmp[1]);
-				$filestring = $tmp[1];
-				$output .= "  var ". $outputBoardId ." = JXG.JSXGraph.loadBoardFromString('". $outputDivId ."', '". htmlspecialchars(strip_tags($filestring)) ."', 'Geonext');";
-			}
-			// construction by $input
-			else {
-				$output .= $input[1];
-			}
-			$output .= "</script>";
-		}
-	}
+      $text = substr_replace($text, $output, $start, $end-$start);
+    }
+  }
 
-	return ($output) ? $output : $text;
+  return $text;
 }
 
 // Add style and scripts
 add_action('wp_head', 'jsxgraph_head');
 
 // We want to run after other filters; hence, a priority of 99.
-add_filter('the_content', 'jsxgraph_filter', 0);
-add_filter('comment_text', 'jsxgraph_filter', 0);
+add_filter('the_content', 'jsxgraph_filter', 99);
+add_filter('comment_text', 'jsxgraph_filter', 99);
 
 ?>
