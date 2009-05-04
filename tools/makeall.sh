3dddd1578a307542ff0476cdeade28e39b2e3ba9
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2009-05-04 07:12:52 +0000

    Release 0.72

--- tools/makeall.sh
+++ tools/makeall.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 python makeDistrib.py
 cd ..
-rm -f tools/jsxgraph0.71.zip
+rm -f tools/jsxgraph0.72.zip
 #cp  src/jsxturtle.js distrib
-zip tools/jsxgraph0.71.zip src/*.js src/COPYING src/COPYING.LESSER distrib/jsxgraphcore.js distrib/jsxgraph.css distrib/prototype.js tools/makeDistrib.py
+zip tools/jsxgraph0.72.zip src/*.js src/COPYING src/COPYING.LESSER distrib/jsxgraphcore.js distrib/jsxgraph.css distrib/prototype.js tools/makeDistrib.py
 cd tools
+echo "makeDoc"
 sh makedoc.sh
