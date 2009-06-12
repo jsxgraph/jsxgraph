eaea9d7f015589b5d583b25a72eb4451050d669c
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2009-06-12 12:48:17 +0000

    Overhaul of the jsdoc-script.

--- tools/makedoc.sh
+++ tools/makedoc.sh
@@ -1,4 +1,9 @@
 ROOT=~/public_html/jsxgraph/jsdoc-toolkit
 FILELIST=$(cat ../src/loadjsxgraph.js | grep "baseFiles\s*=\s*'\(\w*,\)\+" | awk -F \' '{ print $2 }' | sed 's/,/.js ..\/src\//g')
-java -jar $ROOT/jsrun.jar $ROOT/app/run.js -a -t=$ROOT/templates/jsdoc -d=docs ../src/loadjsxgraph.js ../src/$FILELIST.js
+# $FILELIST starts without "../src" and ends without ".js".
+# Further, SVGRenderer and VMLRenderer are not in FILELIST
+# We therefore have to prepend and append "../src/" and ".js":
+#java -jar $ROOT/jsrun.jar $ROOT/app/run.js -a -v -t=$ROOT/templates/jsdoc -d=docs ../src/loadjsxgraph.js ../src/$FILELIST.js ../src/SVGRenderer.js ../src/VMLRenderer.js
+java -jar $ROOT/jsrun.jar $ROOT/app/run.js -a -v -t=$ROOT/templates/jsdoc -d=docs ../src/*.js 
 zip -r docs.zip docs/
+
