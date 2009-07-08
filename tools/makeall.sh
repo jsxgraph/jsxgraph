618dbac558dd01954b72003b5468c157dfcaffd3
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2009-07-08 13:54:29 +0000

    Starting work on version 0.75

--- tools/makeall.sh
+++ tools/makeall.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 python makeDistrib.py
 cd ..
-rm -f tools/jsxgraph0.73.zip
-#cp  src/jsxturtle.js distrib
-zip tools/jsxgraph0.73.zip src/*.js src/COPYING src/COPYING.LESSER distrib/jsxgraphcore.js distrib/jsxgraph.css distrib/prototype.js tools/makeDistrib.py
+rm -f tools/jsxgraph0.74.zip
+zip tools/jsxgraph0.74.zip src/*.js src/COPYING src/COPYING.LESSER distrib/jsxgraphcore.js distrib/jsxgraph.css distrib/prototype.js tools/makeDistrib.py
 cd tools
 echo "makeDoc"
 sh makedoc.sh
