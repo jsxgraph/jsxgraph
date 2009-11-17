e18053c2b46b0b48259da62e4161cf3d24d17e23
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2009-11-17 15:43:42 +0000

    After release of version 0.78a we proceed with the development of version 0.79.

--- src/jsxgraph.js
+++ src/jsxgraph.js
@@ -39,7 +39,7 @@
  */
 JXG.JSXGraph = new function (forceRenderer) {
     var ie, opera, i, arr;
-    this.licenseText = 'JSXGraph v0.78a Copyright (C) see http://jsxgraph.org';
+    this.licenseText = 'JSXGraph v0.79 Copyright (C) see http://jsxgraph.org';
 
     /**
             * Stores the renderer that is used to draw the board.
