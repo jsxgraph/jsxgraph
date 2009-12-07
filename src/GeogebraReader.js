3239a4420ef2b6a421cd91c5a177c930e538a010
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2009-12-07 18:34:26 +0000

    Sliders are recognized if they have a property Value.

--- src/GeogebraReader.js
+++ src/GeogebraReader.js
@@ -191,7 +191,8 @@ this.ggbAct = function(type, m, n, p) {
             JXG.GeogebraReader.debug("regged: "+ v1 +" (id: "+ JXG.GeogebraReader.board.ggbElements[v1].id +")");
         }
         var a = JXG.GeogebraReader.board.ggbElements[v1];
-        if (a.type == JXG.OBJECT_TYPE_SLIDER) {
+        //if (a.type == JXG.OBJECT_TYPE_SLIDER) {
+        if (a.Value != 'undefined') {
           return 'JXG.getReference(JXG.GeogebraReader.board, "'+ v1 +'").Value()';
         } else if (a.elementClass == JXG.OBJECT_TYPE_POLYGON) {
           return 'JXG.getReference(JXG.GeogebraReader.board, "'+ v1 +'").Area()';
