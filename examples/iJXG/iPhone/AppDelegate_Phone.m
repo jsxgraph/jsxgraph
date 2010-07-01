aca876fe95923c259aaa34b964a6f05afc9e33c4
Peter Wilfahrt <peter@techi.de>
2010-07-01 16:11:11 +0000

    even simpler function plotter: test iphone/ipad app

---
+++ examples/iJXG/iPhone/AppDelegate_Phone.m
@@ -0,0 +1,32 @@
+#import "AppDelegate_Phone.h"
+
+@implementation AppDelegate_Phone
+
+@synthesize window;
+
+@synthesize webView;		// Synthesize webView
+
+#define WWW_ROOT	@"www"	// Define the root of the Web folder
+
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+	
+	// Add code to open bundled Web site
+	
+	NSString *path = [[NSBundle mainBundle]
+					  pathForResource:@"index"
+					  ofType:@"html"
+					  inDirectory:WWW_ROOT ];
+	
+	NSURL *url = [NSURL fileURLWithPath:path];
+	
+	NSURLRequest *request = [NSURLRequest requestWithURL:url];
+	
+	[webView loadRequest:request];
+	
+    // Override point for customization after application launch
+	
+    [window makeKeyAndVisible];
+	
+	return YES;
+}
