c1d7fe2a4f59efc10fbc763add183aad6f03ef59
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2011-03-15 15:50:20 +0000

    Adapting the options file of new_examples

--- examples/new_examples/js/jsxgraph-dark.js
+++ examples/new_examples/js/jsxgraph-dark.js
@@ -44,8 +44,17 @@ JXG.extend(JXG.Options, {
         highlightStrokeOpacity: 1,
         fillOpacity: 1,
         highlightFillOpacity: 1,
-        strokeWidth: '2px',
-	    withLabel: false,
+        strokeWidth: 2,
+        highlightStrokeWidth: 2,         
+        withLabel: false,
+        fixed: false,
+        frozen: false,
+        visible: true,
+        layer: 9,
+        dash: 0,
+        shadow: false,
+        trace: false,
+        needsRegularUpdate: true,           
 
         draft : {
             draft : false,
@@ -73,10 +82,23 @@ JXG.extend(JXG.Options, {
         lastArrow : false,
         straightFirst : true,
         straightLast : true,
-        fillColor : '#000000',
+        fillColor : 'none',
         highlightFillColor : 'none',
         strokeColor : '#4096EE',
         highlightStrokeColor : '#45a1ff',
+        withTicks: false,
+        point1 : {                  // Default values for point1 if created by line
+            visible: false, 
+            withLabel: false, 
+            fixed:true,
+            name: ''
+        },
+        point2 : {                  // Default values for point2 if created by line
+            visible: false, 
+            withLabel: false, 
+            fixed: true,
+            name: ''
+        },
 
         ticks : {
             drawLabels : true,
@@ -93,22 +115,132 @@ JXG.extend(JXG.Options, {
     },
 
     axis : {
+        needsRegularUpdate : false,         // Axes only updated after zooming and moving of the origin.
+        strokeWidth: 1,
         strokeColor : '#aaa',
-        highlightStrokeColor : '#aaa'
+        highlightStrokeColor : '#aaa',
+        withTicks: true,
+        straightFirst : true,
+        straightLast : true,
+        lastArrow: true,
+        withLabel: false, 
+        /* line ticks options */
+        ticks : {
+            needsRegularUpdate : false,            
+            strokeWidth: 1,
+            strokeColor : '#666666',
+            highlightStrokeColor : '#888888',
+            drawLabels : true,
+            drawZero : true,
+            insertTicks : false,
+            minTicksDistance : 50,
+            maxTicksDistance : 300,
+            minorHeight : 4,          // if <0: full width and height
+            majorHeight : -1,         // if <0: full width and height
+            minorTicks : 4,
+            defaultDistance : 1,
+            strokeOpacity : 0.25
+        },
+        point1 : {                  // Default values for point1 if created by line
+            needsRegularUpdate : false
+        },
+        point2 : {                  // Default values for point2 if created by line
+            needsRegularUpdate : false
+        }
+    },
+
+    /* special slider options */
+    slider : {
+        snapWidth: null,
+        firstArrow : false,
+        lastArrow : false,
+        withTicks: true,
+        withLabel: true,
+        point1: {
+            needsRegularUpdate : false,
+            showInfobox: false,
+            withLabel: false,
+            visible: false,
+            fixed: true,
+            name: ''
+        },
+        point2: {
+            needsRegularUpdate : false,
+            showInfobox: false,
+            withLabel: false,
+            visible: false,
+            fixed: true,
+            name: ''
+        },
+        glider: {
+            showInfobox: false,
+            name : '',
+            withLabel: false,
+            visible: true,
+            strokeColor : '#666666',
+            highlightStrokeColor : '#888888',
+            fillColor : '#666666',
+            highlightFillColor : 'none',
+            size: 6,
+            name: ''
+        },
+        segment1: {
+            needsRegularUpdate : false,
+            name : '',
+            strokeWidth: 1,
+            strokeColor : '#444444',
+            highlightStrokeColor : '#888888'
+        },
+        segment2: {
+            strokeWidth: 3,
+            name : '',
+            strokeColor : '#444444',
+            highlightStrokeColor : '#888888'
+        },
+        /* line ticks options */
+        ticks : {
+            needsRegularUpdate : false,
+            drawLabels : false,
+            drawZero : true,
+            insertTicks : true,
+            minorHeight : 4,          // if <0: full width and height
+            majorHeight : 10,        // if <0: full width and height
+            minorTicks : 0,
+            defaultDistance : 1,
+            opacity : 1,
+            strokeWidth: 1,
+            strokeColor : '#444444'
+        }, 
+        text: {
+            strokeColor:'#ffffff'
+        }
     },
 
     circle : {
         fillColor : 'none',
         highlightFillColor : 'none',
         strokeColor : '#0000ff',
-        highlightStrokeColor : '#C3D9FF'
+        highlightStrokeColor : '#C3D9FF',
+        center: {
+            visible: false,
+            withLabel: false,
+            fixed: true,
+            name: ''
+        }
     },
 
     conic : {
         fillColor : 'none',
         highlightFillColor : 'none',
         strokeColor : '#0000ff',
-        highlightStrokeColor : '#C3D9FF'
+        highlightStrokeColor : '#C3D9FF',
+        foci: {
+            // points
+            fixed: true,
+            visible: false,
+            withLabel: false,
+            name: ''
+        }
     },
 
     angle : {
@@ -119,7 +251,12 @@ JXG.extend(JXG.Options, {
         strokeColor : '#FF7F00',
         textColor : '#0000FF',
         fillOpacity : 0.3,
-        highlightFillOpacity : 0.3
+        highlightFillOpacity : 0.3,
+        point: {
+            withLabel: false,
+            visible: false,
+            name: ''
+        }
     },
 
     /* special arc options */
@@ -129,7 +266,13 @@ JXG.extend(JXG.Options, {
         fillColor : 'none',
         highlightFillColor : 'none',
         strokeColor : '#0000ff',
-        highlightStrokeColor : '#C3D9FF'
+        highlightStrokeColor : '#C3D9FF',
+        center: {
+            visible: false,
+            withLabel: false,
+            fixed: true,
+            name: ''
+        }
     },
 
     /* special polygon options */
@@ -137,7 +280,19 @@ JXG.extend(JXG.Options, {
         fillColor : '#6BBA70',
         highlightFillColor : '#6BBA70',
         fillOpacity : 0.3,
-        highlightFillOpacity : 0.3
+        highlightFillOpacity : 0.3,
+        withLines: true,
+        lines: {
+            withLabel: false,
+            // Polygon layer + 1
+            layer: 5
+        },
+        points : {                    
+            withLabel: true,
+            strokeColor: '#ff0000',
+            fillColor: '#ff0000',
+            fixed: true
+        }
     },
 
     /* special sector options */
@@ -151,10 +306,13 @@ JXG.extend(JXG.Options, {
     /* special text options */
     text : {
         fontSize : 11,
+        digits: 2,
+        isLabel: false,
         strokeColor : 'gray',
         useASCIIMathML : false,
         useMathJax : false,
-        defaultDisplay : 'html' //'html' or 'internal'
+        defaultDisplay : 'html', //'html' or 'internal'
+        withLabel: false
     },
 
     /* special curve options */
