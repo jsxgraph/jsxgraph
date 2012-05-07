e0480a500f2a209a7d189e03ab7a6aabf775cbdc
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2012-05-07 08:59:47 +0000

    Bug fix: gliders on lines defined by ideal points. Fixing a continuity issue.

--- src/Point.js
+++ src/Point.js
@@ -255,7 +255,7 @@ JXG.extend(JXG.Point.prototype, /** @lends JXG.Point.prototype */ {
                         d = p2c[i];
                     }
                     d = (this.coords.usrCoords[i] - p1c[i]) / d;
-                    sgn = (d>=0) ? 1.0 : -1;
+                    sgn = (d>=0) ? 1 : -1;
                     d = Math.abs(d);
                     this.position = sgn * d/(d+1);
                 } else if (Math.abs(p1c[0])<JXG.Math.eps) {          // The first point is an ideal point
@@ -266,9 +266,11 @@ JXG.extend(JXG.Point.prototype, /** @lends JXG.Point.prototype */ {
                         d = p1c[i];
                     }
                     d = (this.coords.usrCoords[i] - p2c[i]) / d;
-                    sgn = (d>=0) ? 1.0 : -1;
-                    d = Math.abs(d);
-                    this.position = sgn * 1/(d+1);
+                    if (d<0.0) {
+                        this.position = -d + 1;
+                    } else {
+                        this.position = 1/(d+1);
+                    }
                 } else {
                     i = 1;
                     d = p2c[i] - p1c[i];
@@ -322,7 +324,7 @@ JXG.extend(JXG.Point.prototype, /** @lends JXG.Point.prototype */ {
     * @private
     */
     updateGliderFromParent: function() {
-        var p1c, p2c, r, d;
+        var p1c, p2c, r, lbda;
 
         if(this.slideObject.elementClass == JXG.OBJECT_CLASS_CIRCLE) {
 			r = this.slideObject.Radius();
@@ -331,34 +333,39 @@ JXG.extend(JXG.Point.prototype, /** @lends JXG.Point.prototype */ {
 					this.slideObject.center.Y() + r*Math.sin(this.position)
 				]);
         } else if(this.slideObject.elementClass == JXG.OBJECT_CLASS_LINE) {
-            p1c = this.slideObject.point1.coords;
-            p2c = this.slideObject.point2.coords;
-            d = p1c.distance(JXG.COORDS_BY_USER, p2c);
-            p1c = p1c.usrCoords.slice(0);
-            p2c = p2c.usrCoords.slice(0);
-           /* 
-if (this.name=="x") { console.log(this.position+" "+
-    d+" "+ 
-p2c+" "+
-JXG.Math.Geometry.distance([0,0,0], p2c) + ";;;;"+
-    (p1c[1] + this.position*(p2c[1] - p1c[1]))+" "+ 
-    ( p1c[2] + this.position*(p2c[2] - p1c[2])) ); 
-}
-            if (d==Number.POSITIVE_INFINITY) {                       // At least one point is an ideal point
-				d = 1.0/JXG.Math.eps;
-                if (Math.abs(p2c[0])<JXG.Math.eps) {                 // The second point is an ideal point
-                    d /= JXG.Math.Geometry.distance([0,0,0], p2c);
-                    p2c = [1, p1c[1]+p2c[1]*d, p1c[2]+p2c[2]*d];
-                } else {                                             // The first point is an ideal point
-                    d /= JXG.Math.Geometry.distance([0,0,0], p1c);
-                    p1c = [1, p2c[1]+p1c[1]*d, p2c[2]+p1c[2]*d];
+            p1c = this.slideObject.point1.coords.usrCoords;
+            p2c = this.slideObject.point2.coords.usrCoords;
+            if (Math.abs(p2c[0])<JXG.Math.eps) {                        // The second point is an ideal point
+                lbda = Math.min(Math.abs(this.position), 1.0-JXG.Math.eps);
+                lbda /= (1.0-lbda);
+                if (this.position < 0) {
+                    lbda *= -1.0;
                 }
-            }*/
-            this.coords.setCoordinates(JXG.COORDS_BY_USER, [
-                    p1c[0] + this.position*(p2c[0] - p1c[0]),
-                    p1c[1] + this.position*(p2c[1] - p1c[1]),
-					p1c[2] + this.position*(p2c[2] - p1c[2])
+                this.coords.setCoordinates(JXG.COORDS_BY_USER, [
+                    p1c[0] + lbda*p2c[0],
+                    p1c[1] + lbda*p2c[1],
+					p1c[2] + lbda*p2c[2]
                 ]);
+            } else if (Math.abs(p1c[0])<JXG.Math.eps) {                 // The first point is an ideal point
+                lbda = Math.max(this.position, JXG.Math.eps);
+                if (lbda > 1.0) {
+                    lbda = -lbda + 1;
+                } else {
+                    lbda = (1.0-lbda)/lbda;
+                }
+                this.coords.setCoordinates(JXG.COORDS_BY_USER, [
+                    p2c[0] + lbda*p1c[0],
+                    p2c[1] + lbda*p1c[1],
+					p2c[2] + lbda*p1c[2]
+                ]);
+            } else {
+                lbda = this.position;
+                this.coords.setCoordinates(JXG.COORDS_BY_USER, [
+                    p1c[0] + lbda*(p2c[0]-p1c[0]),
+                    p1c[1] + lbda*(p2c[1]-p1c[1]),
+					p1c[2] + lbda*(p2c[2]-p1c[2])
+                ]);
+            }
         } else if(this.slideObject.type == JXG.OBJECT_TYPE_TURTLE) {
             this.coords.setCoordinates(JXG.COORDS_BY_USER, [this.slideObject.Z(this.position), this.slideObject.X(this.position), this.slideObject.Y(this.position)]);
             this.updateConstraint(); // In case, the point is a constrained glider.
