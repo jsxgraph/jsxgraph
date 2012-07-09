b7ee749fa8d463a9b082d780597cacf05ef705dc
Heiko Vogel <heiko.vogel@birdworx.de>
2012-07-09 14:19:19 +0200

    gallery update

--- distrib/themes/gui.js
+++ distrib/themes/gui.js
@@ -12,7 +12,11 @@ JXG.Options = JXG.deepCopy(JXG.Options, {
 	point: {
 		size: 4,
 		fillColor:   '#c00',
-		strokeColor: '#000'
+		strokeColor: '#000',
+
+		// snap on majorTicks
+		snapX: -1,
+		snapY: -1
 	},
 
     glider : {
@@ -49,3 +53,10 @@ JXG.Options = JXG.deepCopy(JXG.Options, {
 
 	//, renderer: 'canvas' // the draftcurves look very ugly on windows ...
 });
+
+if (JXG.isAndroid || JXG.isApple()) {
+    JXG.Options.curve.RDPsmoothing = false;
+    JXG.Options.curve.numberPointsHigh = 600;
+    JXG.Options.curve.numberPointsLow = 100;
+    JXG.Options.curve.doAdvancedPlot = false;
+}
