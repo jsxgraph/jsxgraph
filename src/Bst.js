b52ebda84e2ac3e38eeac8e590512f3c7d69e052
alfred <alfred.wassermann@uni-bayreuth.de>
2012-08-23 14:12:50 +0200

    BST: bug fixes

--- src/Bst.js
+++ src/Bst.js
@@ -6,6 +6,9 @@ JXG.BST = function() {
     
 };    
 
+/**
+ * public
+ */
 JXG.BST.prototype.newNode = function(it, le, ri, n) {
     return {item:it, l:le, r: ri, N: n};
 };
@@ -19,6 +22,42 @@ JXG.BST.prototype.count = function() {
     return this.head.N;
 };
 
+JXG.BST.prototype.search = function(val) {
+    return this.searchR(this.head, val);
+};
+
+JXG.BST.prototype.insert = function(item) {
+    this.head = this.insertR(this.head, item);
+};
+
+JXG.BST.prototype.traverse = function(h, visit) {
+    if (h == this.z) {
+        return;
+    }
+    visit(h);
+    this.traverse(h.l, visit);
+    this.traverse(h.r, visit);
+};
+
+JXG.BST.prototype.insertHead = function(item) {
+    this.head = this.insertT(this.head, item);
+};
+
+JXG.BST.prototype.deleteNode = function(v) {
+    this.head = this.deleteR(this.head, v);
+};
+
+JXG.BST.prototype.balance = function() {
+    this.head = this.balanceR(this.head);
+};
+
+JXG.BST.prototype.show = function() {
+    this.showR(this.head, 0);
+};    
+
+/**
+ * privat
+ */
 JXG.BST.prototype.searchR = function(h, val) {
     var t = h.item;         // <-------
     if (h == this.z) {
@@ -32,10 +71,6 @@ JXG.BST.prototype.searchR = function(h, val) {
     } else {
         return this.searchR(h.r, val);
     }
-};
-
-JXG.BST.prototype.search = function(val) {
-    return this.searchR(this.head, val);
 };
 
 JXG.BST.prototype.insertR = function(h, item) {
@@ -54,11 +89,6 @@ JXG.BST.prototype.insertR = function(h, item) {
     h.N++;
     return h;
 };
-
-JXG.BST.prototype.insert = function(item) {
-    this.head = this.insertR(this.head, item);
-};
-
 
 JXG.BST.prototype.insertRR= function(h, item) {
     var v = item,
@@ -81,32 +111,24 @@ JXG.BST.prototype.insertRR= function(h, item) {
     return h;
 };
 
-JXG.BST.prototype.insertRand = function(item) {
-    this.head = this.insertRR(this.head, item);
-};
-
-
-
-JXG.BST.prototype.traverse = function(h, visit) {
-    if (h == this.z) {
-        return;
-    }
-    visit(h);
-    this.traverse(h.l, visit);
-    this.traverse(h.r, visit);
-};
-
 JXG.BST.prototype.rotR = function(h) {
     var x = h.l;
     h.l = x.r;
     x.r = h;
+
+    h.N = h.l.N + h.r.N + 1;
+    x.N = x.l.N + x.r.N + 1;
     return x;
 };
 
 JXG.BST.prototype.rotL = function(h) {
-    var x = h.r;
+    var x = h.r, 
+        n = x.N;
     h.r = x.l;
     x.l = h;
+    
+    h.N = h.l.N + h.r.N + 1;
+    x.N = x.l.N + x.r.N + 1;
     return x;
 };
 
@@ -126,21 +148,18 @@ JXG.BST.prototype.insertT = function(h, item) {
     return h;
 };
 
-JXG.BST.prototype.insertHead = function(item) {
-    this.head = this.insertT(this.head, item);
-};
-
 JXG.BST.prototype.partR = function(h, k) {
     var t = h.l.N;
     
-    //t = (h==0) ? 0 : h.l.N;    
     if (t > k) {
         h.l = this.partR(h.l, k);
         h = this.rotR(h);
+        this.head = h;
     }
     if (t < k) {
         h.r = this.partR(h.r, k-t-1);
         h = this.rotL(h);
+        this.head = h;
     }
     return h;
 };
@@ -152,6 +171,7 @@ JXG.BST.prototype.joinLR = function(a, b) {
     
     b = this.partR(b, 0);
     b.l = a;
+    b.N = b.l.N + b.r.N + 1;
     return b;
 };
 
@@ -170,11 +190,10 @@ JXG.BST.prototype.deleteR = function(h, v) {
         h = this.joinLR(h.l, h.r);
         delete (x);
     }
+    if (h != this.z) { 
+        h.N = h.l.N + h.r.N + 1;
+    }
     return h;
-};
-
-JXG.BST.prototype.deleteNode = function(v) {
-    this.head = this.deleteR(this.head, v);
 };
 
 JXG.BST.prototype.join = function(a, b) {
@@ -194,36 +213,35 @@ JXG.BST.prototype.balanceR = function(h) {
         return h;
     }
 
-    h = this.partR(h, h.N/2);
+    h = this.partR(h, Math.floor(h.N/2));
+    
     h.l = this.balanceR(h.l);
     h.r = this.balanceR(h.r);
     return h;
 }
 
+
 // ------------------------------------
 
-JXG.BST.prototype.printnode = function(it, hgt) {
+JXG.BST.prototype.printnode = function(node, hgt) {
     var i, t='';
     for (i=0; i<hgt; i++) {
         t += ' ';
     }
-    t += it;
+    t += '('+node.item + ',' + node.N+')';
     console.log(t);
 };
 
 JXG.BST.prototype.showR = function(x, hgt) {
     if (x==this.z) {
-        this.printnode('*', hgt);
+        this.printnode(x, hgt);
         return;
     }
     this.showR(x.r, hgt+1);
-    this.printnode(x.item, hgt);
+    this.printnode(x, hgt);
     this.showR(x.l, hgt+1);
 };
 
-JXG.BST.prototype.show = function() {
-    this.showR(this.head, 0);
-};    
 
 
 
