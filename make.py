457a066a0c82d7cf73ef4919ab9c21fb951aa41d
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2012-12-05 13:29:24 +0100

    Add second license to make.py Release target update jsxgraphcore

--- make.py
+++ make.py
@@ -358,14 +358,15 @@ def makeRelease():
     shutil.copy(output + "/jsxgraphsrc.js", "tmp/jsxgraphsrc.js")
     shutil.copy("README.md", "tmp/README.md")
     shutil.copy("CHANGELOG.md", "tmp/CHANGELOG.md")
-    shutil.copy("LICENSE", "tmp/LICENSE")
+    shutil.copy("LICENSE.MIT", "tmp/LICENSE.MIT")
+    shutil.copy("LICENSE.LGPL", "tmp/LICENSE.LGPL")
     shutil.copy("distrib/jsxgraph.css", "tmp/jsxgraph.css")
     
     makeReaders()
     
     shutil.copy("src/themes/dark.js", "tmp/themes/dark.js")
     shutil.copy("src/themes/gui.js", "tmp/themes/gui.js")
-    os.system("cd tmp && zip -r jsxgraph-" + version + ".zip docs/ jsxgraphcore.js jsxgraphsrc.js jsxgraph.css themes/ README LICENSE && cd ..")
+    os.system("cd tmp && zip -r jsxgraph-" + version + ".zip docs/ jsxgraphcore.js jsxgraphsrc.js jsxgraph.css themes/ README LICENSE.MIT LICENSE.LGPL && cd ..")
     shutil.move("tmp/jsxgraph-" + version + ".zip", output + "/jsxgraph-" + version + ".zip")
 
 '''
