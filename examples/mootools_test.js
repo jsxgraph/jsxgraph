35d76ee1a8633d76144365206c3d89b386c3a9d2
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2013-01-22 15:59:10 +0100

    remove unnecessary and unfixable examples adjust remaining examples to the new folder structure

--- examples/mootools_test.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var Type = function (a, b) {};
-    this.$constructor = new Type('Window', function(){});
-    var test = this.$constructor;
-    this.WindowXYZ = this.$constructor;
-    this.Window = test;
-})();
