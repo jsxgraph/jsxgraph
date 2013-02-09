8250d55e29b0507404a7cdf60532f903422a40cf
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2013-02-09 07:56:46 +0100

    minor refactoring of reader/sketch.js (just added a bunch of curcly braces)

--- src/base/constants.js
+++ src/base/constants.js
@@ -95,7 +95,78 @@
         OBJECT_CLASS_CIRCLE: 3,
         OBJECT_CLASS_CURVE: 4,
         OBJECT_CLASS_AREA: 5,
-        OBJECT_CLASS_OTHER: 6
+        OBJECT_CLASS_OTHER: 6,
+
+        // SketchReader constants
+        GENTYPE_ABC: 1, // unused
+        GENTYPE_AXIS: 2,
+        GENTYPE_MID: 3,
+        GENTYPE_REFLECTION: 4,
+        GENTYPE_MIRRORPOINT: 5,
+        GENTYPE_TANGENT: 6,
+        GENTYPE_PARALLEL: 7,
+        GENTYPE_BISECTORLINES: 8,
+        GENTYPE_PERPENDICULAR_BISECTOR: 9,
+        GENTYPE_BISECTOR: 10,
+        GENTYPE_NORMAL: 11,
+        GENTYPE_POINT: 12,
+        GENTYPE_GLIDER: 13,
+        GENTYPE_INTERSECTION: 14,
+        GENTYPE_CIRCLE: 15,
+        GENTYPE_CIRCLE2POINTS: 16,
+        GENTYPE_LINE: 17,
+        GENTYPE_TRIANGLE: 18,
+        GENTYPE_QUADRILATERAL: 19,
+        GENTYPE_TEXT: 20,
+        GENTYPE_POLYGON: 21,
+        GENTYPE_REGULARPOLYGON: 22,
+        GENTYPE_SECTOR: 23,
+        GENTYPE_ANGLE: 24,
+        GENTYPE_PLOT: 25,
+        GENTYPE_SLIDER: 26,
+        GENTYPE_XYZ: 27, // unused ...
+        GENTYPE_JCODE: 28,
+        GENTYPE_MOVEMENT: 29,
+        GENTYPE_COMBINED: 30,
+        GENTYPE_RULER: 31,
+        // 32 // unused ...
+        GENTYPE_GRID: 33, // obsolete
+        // 34 ... 39 // unused ...
+        GENTYPE_DELETE: 41,
+        GENTYPE_COPY: 42,
+        GENTYPE_MIRROR: 43,
+        GENTYPE_ROTATE: 44,
+        GENTYPE_ABLATION: 45,
+        GENTYPE_MIGRATE: 46,
+        GENTYPE_TRANSFORM: 47,
+        // 48 ... 50 // unused ...
+
+        // Important:
+        // To be able to differentiate between the (GUI-specific) CTX and
+        // (CORE-specific) non-CTX steps, the non-CTX steps must not be changed
+        // to values > 50.
+        GENTYPE_CTX_TYPE_G: 51,
+        GENTYPE_CTX_TYPE_P: 52,
+        GENTYPE_CTX_TRACE: 53,
+        GENTYPE_CTX_VISIBILITY: 54,
+        GENTYPE_CTX_CCVISIBILITY: 55,
+        GENTYPE_CTX_MPVISIBILITY: 56,
+        GENTYPE_CTX_WITHLABEL: 57,
+        GENTYPE_CTX_SETLABEL: 58,
+        GENTYPE_CTX_SETFIXED: 59,
+        GENTYPE_CTX_STROKEWIDTH: 60,
+        GENTYPE_CTX_LABELSIZE: 61,
+        GENTYPE_CTX_SIZE: 62,
+        GENTYPE_CTX_FACE: 63,
+        GENTYPE_CTX_STRAIGHT: 64,
+        GENTYPE_CTX_ARROW: 65,
+        GENTYPE_CTX_COLOR: 66,
+        GENTYPE_CTX_RADIUS: 67,
+        GENTYPE_CTX_COORDS: 68,
+        GENTYPE_CTX_TEXT: 69,
+        GENTYPE_CTX_ANGLERADIUS: 70,
+        GENTYPE_CTX_DOTVISIBILITY: 71,
+        GENTYPE_CTX_FILLOPACITY: 72
     };
 
     JXG.extend(JXG, constants);
