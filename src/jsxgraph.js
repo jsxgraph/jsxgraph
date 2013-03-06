25aa537f3a58dbf80d5d1d2ae9e73dc3c18b8c81
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2013-03-06 15:49:15 +0100

    implements #33

--- src/jsxgraph.js
+++ src/jsxgraph.js
@@ -253,7 +253,7 @@ define([
          * @see JXG.IntergeoReader
          * @see JXG.CinderellaReader
          */
-        loadBoardFromFile: function (box, file, format, attributes) {
+        loadBoardFromFile: function (box, file, format, attributes, callback) {
             var attr, renderer, board, dimensions;
 
             renderer = this.initRenderer(box);
@@ -269,7 +269,7 @@ define([
             board = new Board(box, renderer, '', [150, 150], 1, 1, 50, 50, dimensions.width, dimensions.height, attr);
             board.initInfobox();
 
-            FileReader.parseFileContent(file, board, format);
+            FileReader.parseFileContent(file, board, format, true, callback);
 
             if (board.attr.shownavigation) {
                 board.renderer.drawZoomBar(board);
@@ -293,7 +293,7 @@ define([
          * @see JXG.IntergeoReader
          * @see JXG.CinderellaReader
          */
-        loadBoardFromString: function (box, string, format, attributes) {
+        loadBoardFromString: function (box, string, format, attributes, callback) {
             var attr, renderer, dimensions, board;
 
             renderer = this.initRenderer(box);
@@ -309,7 +309,7 @@ define([
             board = new Board(box, renderer, '', [150, 150], 1.0, 1.0, 50, 50, dimensions.width, dimensions.height, attr);
             board.initInfobox();
 
-            FileReader.parseString(string, board, format, true);
+            FileReader.parseString(string, board, format, true, callback);
 
             if (board.attr.shownavigation) {
                 board.renderer.drawZoomBar(board);
