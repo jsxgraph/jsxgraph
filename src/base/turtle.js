dc928221009501466c9348198bf0b29862aa3c24
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2013-04-30 11:48:08 +0200

    Fix some typos in Turtle method map

--- src/base/turtle.js
+++ src/base/turtle.js
@@ -10,20 +10,20 @@
     This file is part of JSXGraph.
 
     JSXGraph is free software dual licensed under the GNU LGPL or MIT License.
-    
+
     You can redistribute it and/or modify it under the terms of the
-    
+
       * GNU Lesser General Public License as published by
         the Free Software Foundation, either version 3 of the License, or
         (at your option) any later version
       OR
       * MIT License: https://github.com/jsxgraph/jsxgraph/blob/master/LICENSE.MIT
-    
+
     JSXGraph is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
-    
+
     You should have received a copy of the GNU Lesser General Public License and
     the MIT License along with JSXGraph. If not, see <http://www.gnu.org/licenses/>
     and <http://opensource.org/licenses/MIT/>.
@@ -133,7 +133,7 @@ define([
             penUp: 'penUp',
             pu: 'penUp',
             penDown: 'penDown',
-            pd: 'pwnDown',
+            pd: 'penDown',
             clearScreen: 'clearScreen',
             cs: 'clearScreen',
             clean: 'clean',
@@ -143,8 +143,8 @@ define([
             ht: 'hideTurtle',
             showTurtle: 'showTurtle',
             st: 'showTurtle',
-            penSize: 'penSize',
-            penColor: 'penColor',
+            penSize: 'setPenSize',
+            penColor: 'setPenColor',
             pushTurtle: 'pushTurtle',
             push: 'pushTurtle',
             popTurtle: 'popTurtle',
