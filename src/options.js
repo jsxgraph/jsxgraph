857f2e2ea862e7565890c9587413a5239d3ddd93
alfred <alfred.wassermann@uni-bayreuth.de>
2013-07-05 13:45:18 +0200

    Undo an experiment on options (used for bug fix of label jumps)

--- src/options.js
+++ src/options.js
@@ -662,7 +662,7 @@ define([
             /* line ticks options */
             ticks: {
                 label: {
-                    offset: [4, -12 + 8],     // This seems to be a good offset for 12 point fonts
+                    offset: [4, -12 + 3],     // This seems to be a good offset for 12 point fonts
                     parse: false
                 },
                 needsRegularUpdate: false,
