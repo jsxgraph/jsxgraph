67ca6d8cde0eadd5d2b6a8973dd49295aa57e0bb
alfred <alfred.wassermann@uni-bayreuth.de>
2014-08-15 10:48:52 +0200

    Bug fix in loadjsxgraph: isMetroApp was not defined at that point of usage.

--- src/loadjsxgraph.js
+++ src/loadjsxgraph.js
@@ -128,8 +128,12 @@ var JXG = {},
         return inc;
     };
 
+    JXG.isMetroApp = function () {
+        return typeof window === 'object' && window.clientInformation && window.clientInformation.appVersion && window.clientInformation.appVersion.indexOf('MSAppHost') > -1;
+    };
+
     JXG.require = function (libraryName) {
-       //if (JXG.isMetroApp()) { // avoid inline code manipulation in Windows apps -- isMetroApp can't be used it is not yet available at this point
+       if (JXG.isMetroApp()) { // avoid inline code manipulation in Windows apps -- isMetroApp can't be used it is not yet available at this point
             var scriptElement = document.createElement("script");
             var typeAttribute = document.createAttribute("type");
             typeAttribute.nodeValue = "text/javascript";
@@ -139,9 +143,9 @@ var JXG = {},
             scriptElement.setAttributeNode(srcAttribute);
             var headElement = document.getElementsByTagName("head")[0];
             headElement.appendChild(scriptElement);
-        /*} else {
+        } else {
             document.write('<script type="text/javascript" src="' + libraryName + '"><\/script>');
-        }*/
+        }
     };
 
     JXG.baseFiles = 'jxg,base/constants,utils/type,utils/xml,utils/env,utils/event,utils/expect,math/math,math/numerics,math/statistics,math/symbolic,math/geometry,math/poly,math/complex,renderer/abstract,renderer/no,reader/file,parser/geonext,base/board,options,jsxgraph,base/element,base/coords,base/point,base/line,base/group,base/circle,element/conic,base/polygon,base/curve,element/arc,element/sector,base/composition,element/composition,base/text,base/image,element/slider,element/measure,base/chart,base/transformation,base/turtle,utils/color,base/ticks,utils/zip,utils/base64,utils/uuid,utils/encoding,server/server,element/locus,parser/datasource,parser/jessiecode,utils/dump,renderer/svg,renderer/vml,renderer/canvas,renderer/no,element/slopetriangle,math/qdt';
