c6692caf37633c55298389547444762107694a3f
alfred <alfred.wassermann@uni-bayreuth.de>
2015-01-13 16:19:33 +0100

    Try to find out, why GEONEXT labels neglect opacity values.

--- src/reader/geonext.js
+++ src/reader/geonext.js
@@ -167,6 +167,8 @@
             gxtEl.labelColor = rgbo[0];
             gxtEl.withLabel = rgbo[1] > 0;
             gxtEl.labelOpacity = rgbo[1];
+            //gxtEl.labelColor = this.gEBTN(color, 'label');
+            
             gxtEl.colorDraft = JXG.rgba2rgbo(this.gEBTN(color, 'draft'))[0];
 
             // backwards compatibility
