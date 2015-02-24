dcef50f33a7ca35f3c8e0dee51be415398a3dc3d
alfred <alfred.wassermann@uni-bayreuth.de>
2015-02-24 22:30:56 +0100

    Add QUADPACK qk31

--- src/math/numerics.js
+++ src/math/numerics.js
@@ -619,7 +619,7 @@ define(['utils/type', 'math/math'], function (Type, Mat) {
                 fv2[jtw] = fval2;
                 result_gauss += wg[j] * fsum;
                 result_kronrod += wgk[jtw] * fsum;
-                result_abs += wgk[jtw] * (Math.abs (fval1) + Math.abs (fval2));
+                result_abs += wgk[jtw] * (Math.abs(fval1) + Math.abs(fval2));
             }
 
             for (j = 0; j < n / 2; j++) {
@@ -630,14 +630,14 @@ define(['utils/type', 'math/math'], function (Type, Mat) {
                 fv1[jtwm1] = fval1;
                 fv2[jtwm1] = fval2;
                 result_kronrod += wgk[jtwm1] * (fval1 + fval2);
-                result_abs += wgk[jtwm1] * (Math.abs (fval1) + Math.abs (fval2));
+                result_abs += wgk[jtwm1] * (Math.abs(fval1) + Math.abs(fval2));
             }
 
             mean = result_kronrod * 0.5;
             result_asc = wgk[n - 1] * Math.abs (f_center - mean);
 
             for (j = 0; j < n - 1; j++) {
-                result_asc += wgk[j] * (Math.abs (fv1[j] - mean) + Math.abs (fv2[j] - mean));
+                result_asc += wgk[j] * (Math.abs(fv1[j] - mean) + Math.abs(fv2[j] - mean));
             }
 
             // scale by the width of the integration region 
@@ -745,6 +745,68 @@ define(['utils/type', 'math/math'], function (Type, Mat) {
                     ],
                 fv1 = [], fv2 = [];
                 return this._gaussKronrod(interval, f, 11, xgk, wg, wgk, fv1, fv2);
+        },
+
+        GaussKronrod31: function(interval, f) {
+            /* Gauss quadrature weights and kronrod quadrature abscissae and
+                weights as evaluated with 80 decimal digit arithmetic by
+                L. W. Fullerton, Bell Labs, Nov. 1981. */
+
+            var xgk =   /* abscissae of the 21-point kronrod rule */
+                    [
+  0.998002298693397060285172840152271,
+  0.987992518020485428489565718586613,
+  0.967739075679139134257347978784337,
+  0.937273392400705904307758947710209,
+  0.897264532344081900882509656454496,
+  0.848206583410427216200648320774217,
+  0.790418501442465932967649294817947,
+  0.724417731360170047416186054613938,
+  0.650996741297416970533735895313275,
+  0.570972172608538847537226737253911,
+  0.485081863640239680693655740232351,
+  0.394151347077563369897207370981045,
+  0.299180007153168812166780024266389,
+  0.201194093997434522300628303394596,
+  0.101142066918717499027074231447392,
+  0.000000000000000000000000000000000
+                    ],
+
+                /* xgk[1], xgk[3], ... abscissae of the 10-point gauss rule. 
+                xgk[0], xgk[2], ... abscissae to optimally extend the 10-point gauss rule */
+                wg =     /* weights of the 10-point gauss rule */
+                    [
+  0.030753241996117268354628393577204,
+  0.070366047488108124709267416450667,
+  0.107159220467171935011869546685869,
+  0.139570677926154314447804794511028,
+  0.166269205816993933553200860481209,
+  0.186161000015562211026800561866423,
+  0.198431485327111576456118326443839,
+  0.202578241925561272880620199967519
+                    ],
+
+                wgk =   /* weights of the 21-point kronrod rule */
+                    [
+  0.005377479872923348987792051430128,
+  0.015007947329316122538374763075807,
+  0.025460847326715320186874001019653,
+  0.035346360791375846222037948478360,
+  0.044589751324764876608227299373280,
+  0.053481524690928087265343147239430,
+  0.062009567800670640285139230960803,
+  0.069854121318728258709520077099147,
+  0.076849680757720378894432777482659,
+  0.083080502823133021038289247286104,
+  0.088564443056211770647275443693774,
+  0.093126598170825321225486872747346,
+  0.096642726983623678505179907627589,
+  0.099173598721791959332393173484603,
+  0.100769845523875595044946662617570,
+  0.101330007014791549017374792767493
+                    ],
+                fv1 = [], fv2 = [];
+                return this._gaussKronrod(interval, f, 16, xgk, wg, wgk, fv1, fv2);
         },
 
         /**
