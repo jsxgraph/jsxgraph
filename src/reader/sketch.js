1e5b9470a7ecb980b5a614e3bbb50afc5a51f6f6
alfred <alfred.wassermann@uni-bayreuth.de>
2015-03-11 10:32:36 +0100

    sketch reader: show the second vector point.

--- src/reader/sketch.js
+++ src/reader/sketch.js
@@ -701,7 +701,8 @@
                             str1[i] = pn(step.args.p2[i]);
                         }
 
-                        set_str += 'point(' + str1.join(', ') + ') <<id: \'' + pid2 + '\', name: \'\', visible: false, ';
+                        set_str += 'point(' + str1.join(', ') + ') <<id: \'' + pid2 + '\', name: \'\', visible: true, ';
+                        set_str += 'layer: ' + JXG.Options.layer.line + ', size: 2, opacity: 0.2, ';
                         set_str += 'snaptogrid: false, snaptopoints: false, priv: false>>; ';
                         reset_str = 'delete ' + pid2 + '; ' + reset_str;
                     } else {
@@ -1181,7 +1182,8 @@
                     set_str = 'point(' + pn(xstart - step.args.x) + ', ' + pn(ystart - step.args.y) + ') <<id: \'';
                     set_str += step.dest_sub_ids[0] + '\', name: \'\', withLabel: false>>; ';
                     set_str += 'parallelpoint(\'' + step.src_ids[0] + '\',\'' + step.src_ids[1] + '\',\'' + step.dest_sub_ids[0] + '\') <<id: \'' + step.dest_sub_ids[1];
-                    set_str += '\', strokeColor: \'#888888\', visible: false, priv: false, name: \'\', withLabel: false>>; ';
+                    set_str += '\', strokeColor: \'#888888\', visible: true, priv: false, name: \'\', ';
+                    set_str += 'layer: ' + JXG.Options.layer.line + ', size: 2, opacity: 0.2, withLabel: false>>; ';
                     set_str += 'arrow(\'' + step.dest_sub_ids[0] + '\',\'' + step.dest_sub_ids[1]  + '\') <<id: \'' + step.dest_sub_ids[2];
                     set_str += '\', strokeColor: \'#888888\', visible: true, name: \'\', withLabel: false>>; ';
 
