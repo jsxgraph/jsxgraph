39f95bbc1f4aa085e9e525b246f6799fac495683
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-01-31 14:34:59 +0100

    Update intern to 3.0.x
    Adjust configuration accordingly

--- tests/intern.js
+++ tests/intern.js
@@ -37,16 +37,9 @@ define({
     // Name of the tunnel class to use for WebDriver tests
     tunnel: 'SauceLabsTunnel',
 
-    // The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
-    // loader
-    useLoader: {
-        'host-node': 'dojo/dojo',
-        'host-browser': 'node_modules/dojo/dojo.js'
-    },
-
     // Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
     // can be used here
-    loader: {
+    loaderOptions: {
         // Packages that should be registered with the loader in each testing environment
         packages: [
             { name: 'JSXGraph', location: 'src' }
@@ -78,5 +71,10 @@ define({
     functionalSuites: [ /* 'myPackage/tests/functional' */ ],
 
     // A regular expression matching URLs to files that should not be included in code coverage analysis
-    excludeInstrumentation: /^(?:tests|node_modules)\//
+    excludeInstrumentation: /^(?:tests|node_modules)\//,
+
+    reporters: [
+        { id: 'Console' },
+        { id: 'LcovHtml' }
+    ]
 });
