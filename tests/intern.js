1b1e8207537464594b2c312b8cfdd5ca811573bc
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-01-31 21:24:05 +0100

    Start with tests for utils/type

--- tests/intern.js
+++ tests/intern.js
@@ -61,7 +61,7 @@ define({
         //'tests/unit/utils/env',
         //'tests/unit/utils/event',
         //'tests/unit/utils/expect',
-        //'tests/unit/utils/type',
+        'tests/unit/utils/type',
         //'tests/unit/utils/uuid',
         //'tests/unit/utils/xml',
         //'tests/unit/utils/zip',
