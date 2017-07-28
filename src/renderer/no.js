dcea78133cb683d12a1da065ab3deffc1de8c756
alfred <alfred.wassermann@uni-bayreuth.de>
2017-07-28 11:29:14 +0200

    Adapt no-renderer to the last commit

--- src/renderer/no.js
+++ src/renderer/no.js
@@ -490,7 +490,11 @@ define(['jxg', 'renderer/abstract'], function (JXG, AbstractRenderer) {
          * @param {JXG.GeometryElement} element Reference to the object that has to appear.
          * @param {Boolean} value true to show the element, false to hide the element.
          */
-        display: function (element, value) { /* stub */ },
+        display: function (element, value) {
+            if (element) {
+                element.visPropOld.visible = value;
+            }
+        },
 
         /**
          * Shows a hidden element on the canvas; Only a stub, requires implementation in the derived renderer.
