8f7cf50144223647ca3b90ebcaf52361ec186bd2
alfred <alfred.wassermann@uni-bayreuth.de>
2020-11-11 16:53:37 +0100

    Docs for extrapolate.js

--- src/math/extrapolate.js
+++ src/math/extrapolate.js
@@ -42,25 +42,29 @@ define(['jxg', 'math/math', 'utils/type'], function (JXG, Mat, Type) {
     "use strict";
 
     /**
-     * Functions for extrapolation of sequences. Used for finding limits of sequences which is used in functiuon plots.
+     * Functions for extrapolation of sequences. Used for finding limits of sequences which is used for curve plotting.
      * @name JXG.Math.Extrapolate
      * @exports Mat.Extrapolate as JXG.Math.Extrapolate
      * @namespace
      */
     Mat.Extrapolate = {
         /**
+         * Wynn's epsilon algorithm. Ported from the FORTRAN version in
+         * Ernst Joachim Weniger, "Nonlinear sequence transformations for the acceleration of convergence
+         * and the summation of divergent series", Computer Physics Reports Vol. 10, 189-371 (1989).
          *
-         * @param {*} s_n
-         * @param {*} n 
-         * @param {*} e 
+         * @param {Number} s_n next value of sequence, i.e. n-th element of sequence
+         * @param {Number} n index of s_n in the sequence
+         * @param {Array} e One-dimensional array containing the extrapolation data. Has to be supplied by the calling routine.
+         * @returns {Number} New estimate of the limit of the sequence.
+         *
+         * @memberof JXG.Math.Extrapolate
          */
         wynnEps: function(s_n, n, e) {
             var HUGE = 1.e+20,
                 TINY = 1.e-15,
-                f0 = 1,
-                f,
-                j,
-                aux1, aux2, diff, estlim;
+                f0 = 1, // f0 may be changed to other values, see vanden Broeck, Schwartz (1979)
+                f, j, aux1, aux2, diff, estlim;
 
             e[n] = s_n;
             if (n === 0) {
@@ -78,17 +82,23 @@ define(['jxg', 'math/math', 'utils/type'], function (JXG, Mat, Type) {
                         e[j - 1] = aux1 * f + 1 / diff;
                     }
                 }
-// console.log(n % 2, e[0], e[1], e[n%2]);
                 estlim = e[n % 2];
             }
 
-            // if (Math.abs(estlim) > 0.01 * HUGE) {
-            //     estlim = lastval;
-            // }
-            // lasteps = Math.abs(estlim - lastval);
             return estlim;
         },
 
+        /**
+         * Levin transformation. See Numerical Recipes, ed. 3.
+         * Not yet ready for use.
+         *
+         * @param {Number} s_n next value of sequence, i.e. n-th element of sequence
+         * @param {Number} n index of s_n in the sequence
+         * @param {Array} numer One-dimensional array containing the extrapolation data for the numerator. Has to be supplied by the calling routine.
+         * @param {Array} denom One-dimensional array containing the extrapolation data for the denominator. Has to be supplied by the calling routine.
+         *
+         * @memberof JXG.Math.Extrapolate
+        */
         levin: function(s_n, n, numer, denom) {
             var term, fact, j,
                 ratio,
@@ -139,10 +149,22 @@ define(['jxg', 'math/math', 'utils/type'], function (JXG, Mat, Type) {
         //     return estlim;
         // },
 
+        /**
+         * Aitken transformation. Ported from the FORTRAN version in
+         * Ernst Joachim Weniger, "Nonlinear sequence transformations for the acceleration of convergence
+         * and the summation of divergent series", Computer Physics Reports Vol. 10, 189-371 (1989).
+         *
+         * @param {Number} s_n next value of sequence, i.e. n-th element of sequence
+         * @param {Number} n index of s_n in the sequence
+         * @param {Array} a One-dimensional array containing the extrapolation data. Has to be supplied by the calling routine.
+         * @returns {Number} New estimate of the limit of the sequence.
+         *
+         * @memberof JXG.Math.Extrapolate
+         */
         aitken: function(s_n, n, a) {
             var estlim,
                 HUGE = 1.e+20,
-                TINY = 1.e-20,
+                TINY = 1.e-15,
                 denom, v,
                 lowmax, j, m;
 
@@ -166,10 +188,22 @@ define(['jxg', 'math/math', 'utils/type'], function (JXG, Mat, Type) {
             return estlim;
         },
 
+        /**
+         * Iterated Brezinski transformation. Ported from the FORTRAN version in
+         * Ernst Joachim Weniger, "Nonlinear sequence transformations for the acceleration of convergence
+         * and the summation of divergent series", Computer Physics Reports Vol. 10, 189-371 (1989).
+         *
+         * @param {Number} s_n next value of sequence, i.e. n-th element of sequence
+         * @param {Number} n index of s_n in the sequence
+         * @param {Array} a One-dimensional array containing the extrapolation data. Has to be supplied by the calling routine.
+         * @returns {Number} New estimate of the limit of the sequence.
+         *
+         * @memberof JXG.Math.Extrapolate
+         */
         brezinski: function(s_n, n, a) {
             var estlim,
                 HUGE = 1.e+20,
-                TINY = 1.e-20,
+                TINY = 1.e-15,
                 denom,
                 d0, d1, d2,
                 lowmax, j, m;
@@ -197,7 +231,25 @@ define(['jxg', 'math/math', 'utils/type'], function (JXG, Mat, Type) {
             return estlim;
         },
 
-        _limit_iterate: function(x0, h0, f, method, step_type) {
+        /**
+         * Extrapolated iteration to approximate the value f(x_0).
+         *
+         * @param {Number} x0 Value for which the limit of f is to be determined. f(x0) may or may not exist.
+         * @param {Number} h0 Initial (signed) distance from x0.
+         * @param {Function} f Function for which the limit at x0 is to be determined
+         * @param {String} method String to choose the method. Available values: "wynnEps", "aitken", "brezinski"
+         * @param {Number} step_type Approximation method. step_type = 0 uses the sequence x0 + h0/n; step_type = 1 uses the sequence x0 + h0 * 2^(-n)
+         *
+         * @returns {Array} Array of length 3. Position 0: estimated value for f(x0), position 1: 'finite', 'infinite', or 'NaN'.
+         * Position 2: value between 0 and 1 judging the reliability of the result (1: high, 0: not successful).
+         *
+         * @memberof JXG.Math.Extrapolate
+         * @see JXG.Math.Extrapolate.limit
+         * @see JXG.Math.Extrapolate.wynnEps
+         * @see JXG.Math.Extrapolate.aitken
+         * @see JXG.Math.Extrapolate.brezinski
+         */
+        iteration: function(x0, h0, f, method, step_type) {
             var n, v, w,
                 estlim = NaN,
                 delta,
@@ -216,16 +268,13 @@ define(['jxg', 'math/math', 'utils/type'], function (JXG, Mat, Type) {
 
                 w = this[method](v, n - 1, E);
 
-//console.log(n, h, v, w, w / v);
                 if (isNaN(w)) {
                     result = 'NaN';
-//console.log("nan");
                     break;
                 }
                 if (v !== 0 && Math.abs(w / v) > infty) {
                     estlim = Math.abs(w) * Math.sign(v);
                     result = 'infinite';
-//console.log("inf", v, w);
                     break;
                 }
                 delta = w - estlim;
@@ -237,6 +286,52 @@ define(['jxg', 'math/math', 'utils/type'], function (JXG, Mat, Type) {
             return [estlim, result, 1 - (n - 1) / up];
         },
 
+        /**
+         *
+         * @param {Number} x0 Value for which the limit of f is to be determined. f(x0) may or may not exist.
+         * @param {Number} h0 Initial (signed) distance from x0.
+         * @param {Function} f Function for which the limit at x0 is to be determined
+         *
+         * @returns {Array} Array of length 3. Position 0: estimated value for f(x0), position 1: 'finite', 'infinite', or 'NaN'.
+         * Position 2: value between 0 and 1 judging the reliability of the result (1: high, 0: not successful).
+         * In case that the extrapolation fails, position 1 and 2 contain 'direct' and 0.
+         *
+         * @example
+         * var f1 = (x) => Math.log(x),
+         *     f2 = (x) => Math.tan(x - Math.PI * 0.5),
+         *     f3 = (x) => 4 / x;
+         *
+         * var x0 = 0.0000001;
+         * var h = 0.1;
+         * for (let f of [f1, f2, f3]) {
+         *     console.log("x0=", x0, f.toString());
+         *     console.log(JXG.Math.Extrapolate.limit(x0, h, f));
+         *  }
+         *
+         * </pre><div id="JXG5e8c6a7e-eeae-43fb-a669-26b5c9e40cab" class="jxgbox" style="width: 300px; height: 300px;"></div>
+         * <script type="text/javascript">
+         *     (function() {
+         *         var board = JXG.JSXGraph.initBoard('JXG5e8c6a7e-eeae-43fb-a669-26b5c9e40cab',
+         *             {boundingbox: [-8, 8, 8,-8], axis: true, showcopyright: false, shownavigation: false});
+         *     var f1 = (x) => Math.log(x),
+         *         f2 = (x) => Math.tan(x - Math.PI * 0.5),
+         *         f3 = (x) => 4 / x;
+         *
+         *     var x0 = 0.0000001;
+         *     var h = 0.1;
+         *     for (let f of [f1, f2, f3]) {
+         *         console.log("x0=", x0, f.toString());
+         *         console.log(JXG.Math.Extrapolate.limit(x0, h, f));
+         *      }
+         * 
+         *     })();
+         * 
+         * </script><pre>
+         *
+         *
+         * @see JXG.Math.Extrapolate.iteration
+         * @memberof JXG.Math.Extrapolate
+         */
         limit: function(x0, h0, f) {
             var algs = ['wynnEps'], //, 'aitken', 'brezinski'],
                 le = algs.length,
@@ -244,19 +339,14 @@ define(['jxg', 'math/math', 'utils/type'], function (JXG, Mat, Type) {
 
             for (i = 0; i < le; i++) {
                 for (t = 0; t < 2; t++) {
-//console.log(">", algs[i], t)
-                    res = this._limit_iterate(x0, h0, f, algs[i], t);
-//console.log("<", algs[i], res)
+                    res = this.iteration(x0, h0, f, algs[i], t);
                     if (res[2] > 0.6) {
                         return res;
                     }
                 }
             }
-            return [f(x0 + Math.sign(h0) * Math.sqrt(Mat.eps)), 'finitex', 0];
+            return [f(x0 + Math.sign(h0) * Math.sqrt(Mat.eps)), 'direct', 0];
         }
-
-
-
     };
 
     return Mat.Extrapolate;
