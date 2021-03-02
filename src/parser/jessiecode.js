a6ec20b11c9f57411034820cc518bbae1e0fe404
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2021-03-02 22:14:57 +0100

    Bug fix: the previous JessieCode solution was not sufficient

--- src/parser/jessiecode.js
+++ src/parser/jessiecode.js
@@ -1614,7 +1614,11 @@ define([
                     }
                     ret = this.compile(node.children[0], js) + '(' + list.join(', ') + (node.children[2] && js ? ', ' + e : '') + ')' + (node.children[2] && !js ? e : '');
                     if (js) {
-                        ret += ';\n';
+                        // Inserting a newline here allows simulataneously
+                        // - procedural calls like Q.moveTo(...); and
+                        // - function calls in expressions like log(x) + 1;
+                        // Problem: procedural calls will not be ended by a semicolon.
+                        ret += '\n';
                     }
 
                     // save us a function call when compiled to javascript
