73b67dc416f3312f30f60b374b82c3251ea0967d
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2021-11-02 20:56:23 +0100

    Karma: convert testGroup.js

---
+++ test/testGroup.js
@@ -0,0 +1,54 @@
+/*
+    Copyright 2008-2021
+        Matthias Ehmann,
+        Michael Gerhaeuser,
+        Carsten Miller,
+        Bianca Valentin,
+        Alfred Wassermann,
+        Peter Wilfahrt
+
+    This file is part of JSXGraph.
+
+    JSXGraph is free software dual licensed under the GNU LGPL or MIT License.
+
+    You can redistribute it and/or modify it under the terms of the
+
+      * GNU Lesser General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version
+      OR
+      * MIT License: https://github.com/jsxgraph/jsxgraph/blob/master/LICENSE.MIT
+
+    JSXGraph is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License and
+    the MIT License along with JSXGraph. If not, see <http://www.gnu.org/licenses/>
+    and <http://opensource.org/licenses/MIT/>.
+ */
+
+describe("Test JXG.Group", function() {
+    var board, A, B, C;
+
+    document.getElementsByTagName('body')[0].innerHTML = '<div id="jxgbox" style="width: 100px; height: 100px;"></div>';
+    board = JXG.JSXGraph.initBoard('jxgbox', {
+        renderer: 'svg',
+        axis: false,
+        grid: false,
+        boundingbox: [-5, 5, 5, -5],
+        showCopyright: false,
+        showNavigation: false
+    });
+    A = board.create('point', [1, 1]);
+    B = board.create('point', [1, 1]);
+    C = board.create('point', [1, 1]);
+
+    it("Create group", function() {
+        var g = board.create('group', [A, B, C]);
+        expect(g).toBeInstanceOf(JXG.Group);
+    });
+
+});
+
