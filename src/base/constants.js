51f2788fb612165d26bd3f233b014855a0832ab1
alfred <alfred.wassermann@uni-bayreuth.de>
2022-02-01 10:22:45 +0100

    Increase version to 1.4.2dev

--- src/base/constants.js
+++ src/base/constants.js
@@ -42,8 +42,8 @@ define(['jxg'], function (JXG) {
 
     var major = 1,
         minor = 4,
-        patch = 1,
-        add = '', //'dev'
+        patch = 2,
+        add = 'dev', //'dev'
         version = major + '.' + minor + '.' + patch + (add ? '-' + add : ''),
         constants;
 
