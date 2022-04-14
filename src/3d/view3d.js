855f4d35a28ed476321ca115bd0a5ec1a3208774
alfred <alfred.wassermann@uni-bayreuth.de>
2022-04-14 11:21:44 +0200

    3D: add coypright info

--- src/3d/view3d.js
+++ src/3d/view3d.js
@@ -1,9 +1,36 @@
-/*global JXG:true, define: true*/
+/*
+    Copyright 2008-2022
+        Matthias Ehmann,
+        Carsten Miller,
+        Andreas Walter,
+        Alfred Wassermann
 
-"use strict";
+    This file is part of JSXGraph.
+
+    JSXGraph is free software dual licensed under the GNU LGPL or MIT License.
+
+    You can redistribute it and/or modify it under the terms of the
+
+      * GNU Lesser General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version
+      OR
+      * MIT License: https://github.com/jsxgraph/jsxgraph/blob/master/LICENSE.MIT
+
+    JSXGraph is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License and
+    the MIT License along with JSXGraph. If not, see <http://www.gnu.org/licenses/>
+    and <http://opensource.org/licenses/MIT/>.
+ */
+/*global JXG:true, define: true*/
 
 define(['jxg', 'options', 'base/constants', 'utils/type', 'math/math', 'base/element', '3d/threed',
 ], function (JXG, Options, Const, Type, Mat, GeometryElement, ThreeD) {
+    "use strict";
 
     ThreeD.View3D = function (board, parents, attributes) {
         var bbox3d, coords, size;
