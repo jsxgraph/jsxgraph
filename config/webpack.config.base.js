c56ffbf566475fe511d55262fa9b726b0674fa80
Sam Ritchie <sritchie09@gmail.com>
2022-10-14 09:48:10 -0600

    run prettier on config

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  resolve: {
-      fallback: { path: require.resolve("path-browserify"), fs: false },
-      modules: [path.resolve("./src"), path.resolve("./node_modules")],
-      extensions: [".js"]
-    },
+    resolve: {
+        fallback: { path: require.resolve("path-browserify"), fs: false },
+        modules: [path.resolve("./src"), path.resolve("./node_modules")],
+        extensions: [".js"]
+    }
 };
