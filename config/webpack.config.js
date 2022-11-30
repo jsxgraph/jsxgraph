9b7493f6115e74f25b7a0a95e7d676a98d4339dc
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2022-11-30 08:36:01 +0100

    Refactor webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const baseConfig = require("./webpack.config.base");
-const ReplaceInFileWebpackPlugin = require("replace-in-file-webpack-plugin");
+
+// Unused: replace-in-file-webpack-plugin
+// in package.json: "replace-in-file-webpack-plugin": "^1.0.6",
+// const ReplaceInFileWebpackPlugin = require("replace-in-file-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
+// Name of JSXGraph namespace
 const libraryName = "JXG";
 const PATHS = {
     entryPoint: path.resolve(__dirname, "../src/index.js"),
@@ -28,13 +32,26 @@ const config = {
         path: PATHS.bundles,
         filename: "[name].js",
         libraryTarget: "umd",
+
+        // In index.js JSXgrph is exported via "export default JXG"
+        // Without "export: 'default'" below, with
+        //    import pgk from "...jsxgraphcore"
+        // JXG would be available as "pkg.default"
+        // With "export: 'default'" we can do
+        //    import JXG from "...jsxgraphcore"
         library: {
             name: libraryName,
             type: 'var',
             export: 'default'
         },
+
+        // Unnamed AMD define
         umdNamedDefine: false,
+
+        // Webworker and "normal" use
         globalObject: "typeof self !== 'undefined' ? self : this",
+
+        // Just comments
         auxiliaryComment: {
             root: 'Root (browser)',
             commonjs: 'CommonJS',
@@ -44,44 +61,44 @@ const config = {
     },
     target: ["web", "es5"],
 
-    plugins: [
-        new ReplaceInFileWebpackPlugin([
-            {
-                dir: "distrib",
-                files: ["jsxgraphcore.js", "jsxgraphsrc.js"],
-                rules: [
-                    // {
-                    //     search: /\(self,/,
-                    //     replace: "(typeof self !== 'undefined' ? self : this,"
-                    // },
-
-                    // AMD and nodejs
-                    // {
-                    //     search: /return __webpack_exports__;/,
-                    //     replace: "return __webpack_exports__.default;"
-                    // },
-
-                    // AMD does not need canvas
-                    // { search: /\["canvas"\], factory/, replace: "[], factory" },
-
-                    // commonjs
-                    // {
-                    //     search: /\] = factory\(require\("canvas"\)\);/,
-                    //     replace: "] = factory();"
-                    // },
-                    // { search: /factory\(root\["canvas"\]\)/, replace: "factory()" }
-                    // browser
-                    // { 
-                    //     search: /root\["jsxgraphcore"\] = factory\(root\["canvas"\]\);/,
-                    //     replace: "root['JXG'] = factory();"
-                    // }
-                ]
-            }
-        ])
-    ],
+    // Unused for the moment
+    // plugins: [
+    //     new ReplaceInFileWebpackPlugin([
+    //         {
+    //             dir: "distrib",
+    //             files: ["jsxgraphcore.js", "jsxgraphsrc.js"],
+    //             rules: [
+    //                 // {
+    //                 //     search: /\(self,/,
+    //                 //     replace: "(typeof self !== 'undefined' ? self : this,"
+    //                 // },
+    //                 // AMD and nodejs
+    //                 // {
+    //                 //     search: /return __webpack_exports__;/,
+    //                 //     replace: "return __webpack_exports__.default;"
+    //                 // },
+    //                 // AMD does not need canvas
+    //                 // { search: /\["canvas"\], factory/, replace: "[], factory" },
+    //                 // commonjs
+    //                 // {
+    //                 //     search: /\] = factory\(require\("canvas"\)\);/,
+    //                 //     replace: "] = factory();"
+    //                 // },
+    //                 // { search: /factory\(root\["canvas"\]\)/, replace: "factory()" }
+    //                 // browser
+    //                 // {
+    //                 //     search: /root\["jsxgraphcore"\] = factory\(root\["canvas"\]\);/,
+    //                 //     replace: "root['JXG'] = factory();"
+    //                 // }
+    //             ]
+    //         }
+    //     ])
+    // ],
 
     // externals: 'canvas',
     externals: {
+        // AMD does not need canvas, so would root.
+        // commonjs2 needs the canvas plug-in
         canvas: {
             commonjs: 'canvas',
             commonjs2: 'canvas',
