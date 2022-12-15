a81d25519d102dc1d1681646a50080a952ee13e2
Andreas Walter <andreas.walter@uni-bayreuth.de>
2022-12-15 10:53:57 +0100

    strictOrder

--- src/loadjsxgraph.js
+++ src/loadjsxgraph.js
@@ -202,7 +202,7 @@ var JXG = {},
                 requirePathLocation = 'href';
             },
 
-            JSfiles: function (fileArray, preventCaching, root) {
+            JSfiles: function (fileArray, preventCaching, root, strictOrder) {
                 var postfix = '', i, file;
 
                 preventCaching = preventCaching || false;
@@ -213,6 +213,7 @@ var JXG = {},
                 if (root.substr(-1) !== '/') {
                     root += '/';
                 }
+                strictOrder = strictOrder || false;
 
                 for (i = 0; i < fileArray.length; i++) {
                     file = fileArray[i];
@@ -226,8 +227,12 @@ var JXG = {},
                         if (JXG.isMetroApp() || !allowDocumentWrite) {
                             el = createHTMLElement('script', {
                                 type: 'text/javascript',
-                                src: src,
+                                src: src
                             });
+                            if(strictOrder) {
+                                el.defer = true;
+                                el.async = false;
+                            }
                             head = document.getElementsByTagName('head')[0];
                             head.appendChild(el);
                         } else {
