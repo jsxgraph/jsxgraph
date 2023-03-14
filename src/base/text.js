963ad0858e5862dcfe2177e869ade8cfe644302e
Andreas Walter <andreas.walter@uni-bayreuth.de>
2023-03-14 09:23:43 +0100

    adapt convertSketchometry2CSS to new sketchometry font

--- src/base/text.js
+++ src/base/text.js
@@ -824,16 +824,11 @@ JXG.extend(
          */
         convertSketchometry2CSS: function (s) {
             if (Type.isString(s)) {
-                s = s.replace(
-                    /(<|&lt;)sketchofont(>|&gt;)/g,
-                    "<span style=font-family:sketchometry-light;font-weight:500;>"
-                );
-                s = s.replace(/(<|&lt;)\/sketchofont(>|&gt;)/g, "</span>");
-                s = s.replace(
-                    /(<|&lt;)sketchometry-light(>|&gt;)/g,
-                    "<span style=font-family:sketchometry-light;font-weight:500;>"
-                );
-                s = s.replace(/(<|&lt;)\/sketchometry-light(>|&gt;)/g, "</span>");
+                 s = s.replace(
+                 /(<|&lt;)sketchofont(>|&gt;)/g,
+                 "<span class=\"sketcho sketcho-inherit sketcho-"
+                 );
+                 s = s.replace(/(<|&lt;)\/sketchofont(>|&gt;)/g, "\"></span>");
             }
 
             return s;
