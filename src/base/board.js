e40b97a1db20ef7ee48086eceeb9d966dfbdcdff
alfred <alfred.wassermann@uni-bayreuth.de>
2023-03-24 11:27:53 +0100

    Typo in comment

--- src/base/board.js
+++ src/base/board.js
@@ -1326,7 +1326,7 @@ JXG.extend(
 
         /**
          * Moves, rotates and scales a line or polygon with two fingers.
-         * @param {Array} tar Array conatining touch event objects: {JXG.Board#touches.targets}.
+         * @param {Array} tar Array containing touch event objects: {JXG.Board#touches.targets}.
          * @param {object} drag The object that is dragged:
          * @param {Number} id pointerId of the event. In case of old touch event this is emulated.
          */
