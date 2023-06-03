313c233fd5b9607bad8ca383f5c2bc947948a394
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2023-06-03 17:31:56 +0200

    Undo double fix of #544

--- src/renderer/abstract.js
+++ src/renderer/abstract.js
@@ -816,23 +816,23 @@ JXG.extend(
             if (a.evFirst || a.evLast) {
                 d = d1x = d1y = d2x = d2y = 0.0;
 
-                s1 = (Type.evaluate(el.point1.visProp.visible) ? 1 : 0 ) *
-                    (Type.evaluate(el.point1.visProp.size) +
-                    Type.evaluate(el.point1.visProp.strokewidth));
+                s1 = Type.evaluate(el.point1.visProp.size) +
+                    Type.evaluate(el.point1.visProp.strokewidth);
 
-                s2 = (Type.evaluate(el.point2.visProp.visible) ? 1 : 0 ) *
-                    (Type.evaluate(el.point2.visProp.size) +
-                    Type.evaluate(el.point2.visProp.strokewidth));
+                s2 = Type.evaluate(el.point2.visProp.size) +
+                    Type.evaluate(el.point2.visProp.strokewidth);
 
                 // Handle touchlastpoint /touchfirstpoint
-                if (a.evFirst && Type.evaluate(el.visProp.touchfirstpoint) && Type.evaluate(el.point1.visProp.visible)) {
+                if (a.evFirst && Type.evaluate(el.visProp.touchfirstpoint) &&
+                        Type.evaluate(el.point1.visProp.visible)) {
                     d = c1.distance(Const.COORDS_BY_SCREEN, c2);
                     //if (d > s) {
                     d1x = ((c2.scrCoords[1] - c1.scrCoords[1]) * s1) / d;
                     d1y = ((c2.scrCoords[2] - c1.scrCoords[2]) * s1) / d;
                     //}
                 }
-                if (a.evLast && Type.evaluate(el.visProp.touchlastpoint) && Type.evaluate(el.point2.visProp.visible)) {
+                if (a.evLast && Type.evaluate(el.visProp.touchlastpoint) &&
+                        Type.evaluate(el.point2.visProp.visible)) {
                     d = c1.distance(Const.COORDS_BY_SCREEN, c2);
                     //if (d > s) {
                     d2x = ((c2.scrCoords[1] - c1.scrCoords[1]) * s2) / d;
