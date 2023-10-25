e7335451275e78cfc9a06dfc3ab03614cb495cbd
alfred <alfred.wassermann@uni-bayreuth.de>
2023-10-25 17:04:04 +0200

    Add Brent's glomin

--- src/math/numerics.js
+++ src/math/numerics.js
@@ -3827,6 +3827,240 @@ Mat.Numerics = {
     },
 
     /**
+     *
+     *   Purpose:
+     *
+     *   GLOMIN seeks a global minimum of a function F(X) in an interval [A,B].
+     * 
+     * Discussion:
+     *
+     *  This function assumes that F(X) is twice continuously differentiable over [A,B] 
+     * and that F''(X) <= M for all X in [A,B].
+     *  
+     * Licensing:
+     *   This code is distributed under the GNU LGPL license.
+     * 
+     * Modified:
+     * 
+     *   17 April 2008
+     * 
+     * Author:
+     *
+     *   Original FORTRAN77 version by Richard Brent.
+     *   C version by John Burkardt.
+     *   https://people.math.sc.edu/Burkardt/c_src/brent/brent.c
+     *
+     * Reference:
+     *
+     *   Richard Brent,
+     *  Algorithms for Minimization Without Derivatives,
+     *   Dover, 2002,
+     *  ISBN: 0-486-41998-3,
+     *   LC: QA402.5.B74.
+     *
+     * Parameters:
+     *
+     *   Input, double A, B, the endpoints of the interval.
+     *  It must be the case that A < B.
+     * 
+     *   Input, double C, an initial guess for the global
+     *  minimizer.  If no good guess is known, C = A or B is acceptable.
+     * 
+     *  Input, double M, the bound on the second derivative.
+     * 
+     *   Input, double MACHEP, an estimate for the relative machine
+     *  precision.
+     * 
+     *   Input, double E, a positive tolerance, a bound for the
+     *  absolute error in the evaluation of F(X) for any X in [A,B].
+     * 
+     *   Input, double T, a positive error tolerance.
+     * 
+     *    Input, double F (double x ), a user-supplied
+     *  function whose global minimum is being sought.
+     *
+     *   Output, double *X, the estimated value of the abscissa
+     *  for which F attains its global minimum value in [A,B].
+     * 
+     *   Output, double GLOMIN, the value F(X).
+     */
+    glomin: function (f, x0) {
+        var a0, a2, a3, d0, d1, d2, h,
+            k, m2,
+            p, q, qs,
+            r, s, sc,
+            y, y0, y1, y2, y3, yb,
+            z0, z1, z2,
+            a, b, c, x,
+            m = 10000.0,
+            t = Mat.eps, // * Mat.eps,
+            e = Mat.eps * Mat.eps,
+            machep = Mat.eps * Mat.eps;
+
+        a = x0[0];
+        b = x0[1];
+        c = (f(a) < f(b)) ? a : b;
+
+        a0 = b;
+        x = a0;
+        a2 = a;
+        y0 = f(b);
+        yb = y0;
+        y2 = f(a);
+        y = y2;
+
+        if (y0 < y) {
+            y = y0;
+        } else {
+            x = a;
+        }
+
+        if (m <= 0.0 || b <= a) {
+            return y;
+        }
+
+        m2 = 0.5 * (1.0 + 16.0 * machep) * m;
+
+        if (c <= a || b <= c) {
+            sc = 0.5 * (a + b);
+        } else {
+            sc = c;
+        }
+
+        y1 = f(sc);
+        k = 3;
+        d0 = a2 - sc;
+        h = 9.0 / 11.0;
+
+        if (y1 < y) {
+            x = sc;
+            y = y1;
+        }
+
+        for (; ;) {
+            d1 = a2 - a0;
+            d2 = sc - a0;
+            z2 = b - a2;
+            z0 = y2 - y1;
+            z1 = y2 - y0;
+            r = d1 * d1 * z0 - d0 * d0 * z1;
+            p = r;
+            qs = 2.0 * (d0 * z1 - d1 * z0);
+            q = qs;
+
+            if (k < 1000000 || y2 <= y) {
+                for (; ;) {
+                    if (q * (r * (yb - y2) + z2 * q * ((y2 - y) + t)) <
+                        z2 * m2 * r * (z2 * q - r)) {
+
+                        a3 = a2 + r / q;
+                        y3 = f(a3);
+
+                        if (y3 < y) {
+                            x = a3;
+                            y = y3;
+                        }
+                    }
+                    k = ((1611 * k) % 1048576);
+                    q = 1.0;
+                    r = (b - a) * 0.00001 * k;
+
+                    if (z2 <= r) {
+                        break;
+                    }
+                }
+            } else {
+                k = ((1611 * k) % 1048576);
+                q = 1.0;
+                r = (b - a) * 0.00001 * k;
+
+                while (r < z2) {
+                    if (q * (r * (yb - y2) + z2 * q * ((y2 - y) + t)) <
+                        z2 * m2 * r * (z2 * q - r)) {
+
+                        a3 = a2 + r / q;
+                        y3 = f(a3);
+
+                        if (y3 < y) {
+                            x = a3;
+                            y = y3;
+                        }
+                    }
+                    k = ((1611 * k) % 1048576);
+                    q = 1.0;
+                    r = (b - a) * 0.00001 * k;
+                }
+            }
+
+            r = m2 * d0 * d1 * d2;
+            s = Math.sqrt(((y2 - y) + t) / m2);
+            h = 0.5 * (1.0 + h);
+            p = h * (p + 2.0 * r * s);
+            q = q + 0.5 * qs;
+            r = - 0.5 * (d0 + (z0 + 2.01 * e) / (d0 * m2));
+
+            if (r < s || d0 < 0.0) {
+                r = a2 + s;
+            } else {
+                r = a2 + r;
+            }
+
+            if (0.0 < p * q) {
+                a3 = a2 + p / q;
+            } else {
+                a3 = r;
+            }
+
+            for (; ;) {
+                a3 = Math.max(a3, r);
+
+                if (b <= a3) {
+                    a3 = b;
+                    y3 = yb;
+                } else {
+                    y3 = f(a3);
+                }
+
+                if (y3 < y) {
+                    x = a3;
+                    y = y3;
+                }
+
+                d0 = a3 - a2;
+
+                if (a3 <= r) {
+                    break;
+                }
+
+                p = 2.0 * (y2 - y3) / (m * d0);
+
+                if ((1.0 + 9.0 * machep) * d0 <= Math.abs(p)) {
+                    break;
+                }
+
+                if (0.5 * m2 * (d0 * d0 + p * p) <= (y2 - y) + (y3 - y) + 2.0 * t) {
+                    break;
+                }
+                a3 = 0.5 * (a2 + a3);
+                h = 0.9 * h;
+            }
+
+            if (b <= a3) {
+                break;
+            }
+
+            a0 = sc;
+            sc = a2;
+            a2 = a3;
+            y0 = y1;
+            y1 = y2;
+            y2 = y3;
+        }
+
+        return [x, y];
+    },
+
+    /**
      * Implements the Ramer-Douglas-Peucker algorithm.
      * It discards points which are not necessary from the polygonal line defined by the point array
      * pts. The computation is done in screen coordinates.
