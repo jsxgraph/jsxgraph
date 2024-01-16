ca6a799e9a8915ac28433585f88da2be1138efff
alfred <alfred.wassermann@uni-bayreuth.de>
2024-01-16 09:55:12 +0100

    Add license text to mono_thin.js

--- src/themes/mono_thin.js
+++ src/themes/mono_thin.js
@@ -1,3 +1,30 @@
+/*
+    Copyright 2008-2023
+        Matthias Ehmann,
+        Carsten Miller,
+        Alfred Wassermann
+
+    This file is part of JSXGraph.
+
+    JSXGraph is free software dual licensed under the GNU LGPL or MIT License.
+
+    You can redistribute it and/or modify it under the terms of the
+
+      * GNU Lesser General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version
+      OR
+      * MIT License: https://github.com/jsxgraph/jsxgraph/blob/master/LICENSE.MIT
+
+    JSXGraph is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License and
+    the MIT License along with JSXGraph. If not, see <https://www.gnu.org/licenses/>
+    and <https://opensource.org/licenses/MIT/>.
+ */
 import JXG from "../jxg";
 
 JXG.themes['mono_thin'] = function(options) {
