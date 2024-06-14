db6ae93c6b87b0af837c732fb00c714b85e2d6d8
alfred <alfred.wassermann@uni-bayreuth.de>
2024-06-14 15:18:46 +0200

    Start developing the next version

--- src/base/constants.js
+++ src/base/constants.js
@@ -37,8 +37,8 @@ import JXG from "../jxg.js";
 
 var major = 1,
     minor = 9,
-    patch = 1,
-    add = '', // 'dev' 'beta'
+    patch = 2,
+    add = 'dev', // 'dev' 'beta'
     version = major + '.' + minor + '.' + patch + (add ? '-' + add : ''),
     constants;
 
