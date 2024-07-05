aea34eb5084a21b3a42a8ba6c668fd6c0d891c53
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2024-07-05 08:36:50 +0200

    Start work on v1.9.3 dev

--- src/base/constants.js
+++ src/base/constants.js
@@ -37,8 +37,8 @@ import JXG from "../jxg.js";
 
 var major = 1,
     minor = 9,
-    patch = 2,
-    add = '', // 'dev' 'beta'
+    patch = 3,
+    add = 'dev', // 'dev' 'beta'
     version = major + '.' + minor + '.' + patch + (add ? '-' + add : ''),
     constants;
 
