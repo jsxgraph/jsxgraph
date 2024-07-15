jsxgraph/jsxgraph

commit 67a132c0761c9b2e12ed0d97f0044c24125cd116

Code refactoring

src/jsxgraph.js
@@ -172,9 +172,22 @@ JXG.JSXGraph = {
      */
     _setAttributes: function (attributes) {
         // merge attributes
-        var attr = Type.copyAttributes(attributes, Options, "board");
+        var attr = Type.copyAttributes(attributes, Options, 'board'),
 
-        // The attributes which are objects have to be copied separately
+            // These attributes which are objects have to be copied separately
+            list = ['drag', 'fullscreen',
+                'keyboard', 'logging',
+                'navbar', 'pan', 'resize',
+                'screenshot', 'selection',
+                'zoom'],
+            len = list.length, i, key;
+
+        for (i = 0; i < len; i++) {
+            key = list[i];
+            attr[key] = Type.copyAttributes(attr, Options, 'board', key);
+        }
+
+/*
         attr.zoom = Type.copyAttributes(attr, Options, "board", "zoom");
         attr.pan = Type.copyAttributes(attr, Options, "board", "pan");
         attr.drag = Type.copyAttributes(attr, Options, "board", "drag");
@@ -185,6 +198,7 @@ JXG.JSXGraph = {
         attr.resize = Type.copyAttributes(attr, Options, "board", "resize");
         attr.fullscreen = Type.copyAttributes(attr, Options, "board", "fullscreen");
         attr.logging = Type.copyAttributes(attr, Options, 'board', 'logging');
+*/
 
         // Treat moveTarget separately, because deepCopy will not work here.
         // Reason: moveTarget will be an HTML node and it is prevented that Type.deepCopy will copy it.
@@ -286,7 +300,7 @@ JXG.JSXGraph = {
     },
 
     /**
-     * Initialise a new board.
+     * Initialize a new board.
      * @param {String} box HTML-ID to the HTML-element in which the board is painted.
      * @param {Object} attributes An object that sets some of the board properties. Most of these properties can be set via JXG.Options.
      * @param {Array} [attributes.boundingbox=[-5, 5, 5, -5]] An array containing four numbers describing the left, top, right and bottom boundary of the board in user coordinates
@@ -305,13 +319,9 @@ JXG.JSXGraph = {
      * @see JXG.AbstractRenderer#drawZoomBar
      */
     initBoard: function (box, attributes) {
-        var originX, originY,
-            unitX, unitY,
-            renderer,
-            offX = 0,
-            offY = 0,
-            w, h,
-            dimensions, bbox,
+        var originX, originY, unitX, unitY, w, h,
+            offX = 0, offY = 0,
+            renderer, dimensions, bbox,
             attr, axattr, axattr_x, axattr_y,
             board;
 
@@ -606,31 +616,15 @@ JXG.JSXGraph = {
 };
 
 // JessieScript/JessieCode startup:
-// Search for script tags of type text/jessiescript and interprete them.
-if (Env.isBrowser && typeof window === "object" && typeof document === "object") {
-    Env.addEvent(
-        window,
-        "load",
+// Search for script tags of type text/jessiecode and execute them.
+if (Env.isBrowser && typeof window === 'object' && typeof document === 'object') {
+    Env.addEvent(window, 'load',
         function () {
-            var type,
-                i,
-                j,
-                div,
-                id,
-                board,
-                txt,
-                width,
-                height,
-                maxWidth,
-                aspectRatio,
-                cssClasses,
-                bbox,
-                axis,
-                grid,
-                code,
-                src,
-                request,
+            var type, i, j, div, id,
+                board, txt, width, height, maxWidth, aspectRatio,
+                cssClasses, bbox, axis, grid, code, src, request,
                 postpone = false,
+
                 scripts = document.getElementsByTagName("script"),
                 init = function (code, type, bbox) {
                     var board = JXG.JSXGraph.initBoard(id, {

src/renderer/svg.js
@@ -595,15 +595,10 @@ JXG.extend(
         displayCopyright: function (str, fontsize) {
             var node = this.createPrim("text", "licenseText"),
                 t;
-            node.setAttributeNS(null, "x", "20px");
-            node.setAttributeNS(null, "y", 2 + fontsize + "px");
-            node.setAttributeNS(
-                null,
-                "style",
-                "font-family:Arial,Helvetica,sans-serif; font-size:" +
-                fontsize +
-                "px; fill:#356AA0;  opacity:0.3;"
-            );
+            node.setAttributeNS(null, 'x', '20px');
+            node.setAttributeNS(null, 'y', 2 + fontsize + 'px');
+            node.setAttributeNS(null, 'style', 'font-family:Arial,Helvetica,sans-serif; font-size:' +
+                    fontsize + 'px; fill:#356AA0;  opacity:0.3;');
             t = this.container.ownerDocument.createTextNode(str);
             node.appendChild(t);
             this.appendChildPrim(node, 0);

src/base/ticks.js
@@ -628,7 +628,7 @@ JXG.extend(
             // Calculate (signed) distance from Zero to P1 and to P2
             dZeroPoint1 = this.getDistanceFromZero(coordsZero, point1);
             dZeroPoint2 = this.getDistanceFromZero(coordsZero, point2);
-            
+
             // Recompute lengths of arrow heads
             a1 = dZeroPoint1 - a1;
             a2 = dZeroPoint1 - a2;

commit 75b6865119a61035857ac62543ee7aef10dbe4eb

Fix regression: dumpToDataURI
Introduced in 67a132c0761c9b2e12ed0d97f0044c24125cd116 in
jsxgraph.js and a minor inconsistency in svg.js

src/jsxgraph.js
@@ -179,7 +179,7 @@ JXG.JSXGraph = {
                 'drag', 'fullscreen',
                 'intl',
                 'keyboard', 'logging',
-                'navbar', 'pan', 'resize',
+                'pan', 'resize',
                 'screenshot', 'selection',
                 'zoom'
             ],
@@ -189,6 +189,7 @@ JXG.JSXGraph = {
             key = list[i];
             attr[key] = Type.copyAttributes(attr, options, 'board', key);
         }
+        attr.navbar = Type.copyAttributes(attr.navbar, options, "navbar");
 
         // Treat moveTarget separately, because deepCopy will not work here.
         // Reason: moveTarget will be an HTML node and it is prevented that Type.deepCopy will copy it.

src/renderer/svg.js
@@ -2146,8 +2146,7 @@ JXG.extend(
                 }
                 while (svgRoot.nextSibling) {
                     // Copy all value attributes
-                    // Type.concat(values, this._getValuesOfDOMElements(svgRoot.nextSibling));
-                    values = values.concat(this._getValuesOfDOMElements(svgRoot.nextSibling));
+                    Type.concat(values, this._getValuesOfDOMElements(svgRoot.nextSibling));
                     this.foreignObjLayer.appendChild(svgRoot.nextSibling);
                 }
             }