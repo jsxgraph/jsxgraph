223370b437c180b4365ae394829a8514576c1800
alfred <alfred.wassermann@uni-bayreuth.de>
2024-11-14 15:12:26 +0100

    JessieCode: enable numbers in scientific notation

--- src/parser/jessiecode.js
+++ src/parser/jessiecode.js
@@ -3559,126 +3559,128 @@ var YYSTATE=YY_START;
 switch($avoiding_name_collisions) {
 case 0:/* ignore */
 break;
-case 1:return 78
+case 1:return 78  /* New 123.1234e+-12 */
 break;
-case 2:return 78
+case 2:return 78  /* Old 123.1234 or .1234 */
 break;
-case 3: return 77;
+case 3:return 78  /* Old 123 */
 break;
 case 4: return 77;
 break;
-case 5:/* ignore comment */
+case 5: return 77;
 break;
-case 6:/* ignore multiline comment */
+case 6:/* ignore comment */
 break;
-case 7:return 7
+case 7:/* ignore multiline comment */
 break;
-case 8:return 12
+case 8:return 7
 break;
-case 9:return 14
+case 9:return 12
 break;
-case 10:return 17
+case 10:return 14
 break;
-case 11:return 15
+case 11:return 17
 break;
-case 12:return 91
+case 12:return 15
 break;
-case 13:return 93
+case 13:return 91
 break;
-case 14:return 19
+case 14:return 93
 break;
-case 15:return 23
+case 15:return 19
 break;
-case 16:return 21
+case 16:return 23
 break;
-case 17:return 75
+case 17:return 21
 break;
-case 18:return 76
+case 18:return 75
 break;
-case 19:return 74
+case 19:return 76
 break;
-case 20:return 80
+case 20:return 74
 break;
-case 21:return 94
+case 21:return 80
 break;
 case 22:return 94
 break;
-case 23:return 82
+case 23:return 94
 break;
-case 24:return 83
+case 24:return 82
 break;
-case 25:return 26
+case 25:return 83
 break;
-case 26:return 27
+case 26:return 26
 break;
-case 27:return 16
+case 27:return 27
 break;
-case 28:return '#'
+case 28:return 16
 break;
-case 29:return 34
+case 29:return '#'
 break;
-case 30:return 35
+case 30:return 34
 break;
-case 31:return 79
+case 31:return 35
 break;
-case 32:return 64
+case 32:return 79
 break;
-case 33:return 65
+case 33:return 64
 break;
-case 34:return 66
+case 34:return 65
 break;
-case 35:return 8
+case 35:return 66
 break;
-case 36:return 10
+case 36:return 8
 break;
-case 37:return 58
+case 37:return 10
 break;
-case 38:return 57
+case 38:return 58
 break;
 case 39:return 57
 break;
-case 40:return 53
+case 40:return 57
 break;
-case 41:return 54
+case 41:return 53
 break;
-case 42:return 55
+case 42:return 54
 break;
-case 43:return 50
+case 43:return 55
 break;
-case 44:return 51
+case 44:return 50
 break;
-case 45:return 47
+case 45:return 51
 break;
-case 46:return 45
+case 46:return 47
 break;
-case 47:return 48
+case 47:return 45
 break;
-case 48:return 46
+case 48:return 48
 break;
-case 49:return 41
+case 49:return 46
 break;
-case 50:return 43
+case 50:return 41
 break;
-case 51:return 42
+case 51:return 43
 break;
-case 52:return 39
+case 52:return 42
 break;
-case 53:return 37
+case 53:return 39
 break;
-case 54:return 32
+case 54:return 37
 break;
-case 55:return 86
+case 55:return 32
 break;
-case 56:return 5
+case 56:return 86
 break;
-case 57:return 20
+case 57:return 5
 break;
-case 58:return 'INVALID'
+case 58:return 20
+break;
+case 59:return 'INVALID'
 break;
 }
 },
-rules: [/^(?:\s+)/,/^(?:[0-9]+\.[0-9]*|[0-9]*\.[0-9]+\b)/,/^(?:[0-9]+)/,/^(?:"(\\["]|[^"])*")/,/^(?:'(\\[']|[^'])*')/,/^(?:\/\/.*)/,/^(?:\/\*(.|\n|\r)*?\*\/)/,/^(?:if\b)/,/^(?:else\b)/,/^(?:while\b)/,/^(?:do\b)/,/^(?:for\b)/,/^(?:function\b)/,/^(?:map\b)/,/^(?:use\b)/,/^(?:return\b)/,/^(?:delete\b)/,/^(?:true\b)/,/^(?:false\b)/,/^(?:null\b)/,/^(?:Infinity\b)/,/^(?:->)/,/^(?:=>)/,/^(?:<<)/,/^(?:>>)/,/^(?:\{)/,/^(?:\})/,/^(?:;)/,/^(?:#)/,/^(?:\?)/,/^(?::)/,/^(?:NaN\b)/,/^(?:\.)/,/^(?:\[)/,/^(?:\])/,/^(?:\()/,/^(?:\))/,/^(?:!)/,/^(?:\^)/,/^(?:\*\*)/,/^(?:\*)/,/^(?:\/)/,/^(?:%)/,/^(?:\+)/,/^(?:-)/,/^(?:<=)/,/^(?:<)/,/^(?:>=)/,/^(?:>)/,/^(?:==)/,/^(?:~=)/,/^(?:!=)/,/^(?:&&)/,/^(?:\|\|)/,/^(?:=)/,/^(?:,)/,/^(?:$)/,/^(?:[A-Za-z_\$][A-Za-z0-9_]*)/,/^(?:.)/],
-conditions: {"INITIAL":{"rules":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58],"inclusive":true}}
+rules: [/^(?:\s+)/,/^(?:[0-9]*\.?[0-9]+([eE][-+]?[0-9]+))/,/^(?:[0-9]+\.[0-9]*|[0-9]*\.[0-9]+\b)/,/^(?:[0-9]+)/,/^(?:"(\\["]|[^"])*")/,/^(?:'(\\[']|[^'])*')/,/^(?:\/\/.*)/,/^(?:\/\*(.|\n|\r)*?\*\/)/,/^(?:if\b)/,/^(?:else\b)/,/^(?:while\b)/,/^(?:do\b)/,/^(?:for\b)/,/^(?:function\b)/,/^(?:map\b)/,/^(?:use\b)/,/^(?:return\b)/,/^(?:delete\b)/,/^(?:true\b)/,/^(?:false\b)/,/^(?:null\b)/,/^(?:Infinity\b)/,/^(?:->)/,/^(?:=>)/,/^(?:<<)/,/^(?:>>)/,/^(?:\{)/,/^(?:\})/,/^(?:;)/,/^(?:#)/,/^(?:\?)/,/^(?::)/,/^(?:NaN\b)/,/^(?:\.)/,/^(?:\[)/,/^(?:\])/,/^(?:\()/,/^(?:\))/,/^(?:!)/,/^(?:\^)/,/^(?:\*\*)/,/^(?:\*)/,/^(?:\/)/,/^(?:%)/,/^(?:\+)/,/^(?:-)/,/^(?:<=)/,/^(?:<)/,/^(?:>=)/,/^(?:>)/,/^(?:==)/,/^(?:~=)/,/^(?:!=)/,/^(?:&&)/,/^(?:\|\|)/,/^(?:=)/,/^(?:,)/,/^(?:$)/,/^(?:[A-Za-z_\$][A-Za-z0-9_]*)/,/^(?:.)/],
+conditions: {"INITIAL":{"rules":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59],"inclusive":true}}
 });
 return lexer;
 })();
