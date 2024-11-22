bd45e493e91fab8cbb633269646d7ddda7e045b2
Aaron Fenyes <aaron.fenyes@fareycircles.ooo>
2024-11-22 01:48:48 -0800

    Clarify comments about camera axis

--- src/3d/sphere3d.js
+++ src/3d/sphere3d.js
@@ -240,16 +240,17 @@ JXG.extend(
                     inward;
 
                 if (distOffAxis > 1e-8) {
-                    // if the center of the sphere isn't too close to the view
+                    // if the center of the sphere isn't too close to the camera
                     // axis, find the direction in plane of the screen that
-                    // points from the center of the sphere toward the view axis
+                    // points from the center of the sphere toward the camera
+                    // axis
                     inward = [
                         -(p[0] * cam[1][1] + p[1] * cam[2][1]) / distOffAxis,
                         -(p[0] * cam[1][2] + p[1] * cam[2][2]) / distOffAxis,
                         -(p[0] * cam[1][3] + p[1] * cam[2][3]) / distOffAxis
                     ];
                 } else {
-                    // if the center of the sphere is very close to the view
+                    // if the center of the sphere is very close to the camera
                     // axis, choose an arbitrary unit vector in the plane of the
                     // screen
                     inward = [cam[1][1], cam[1][2], cam[1][3]];
