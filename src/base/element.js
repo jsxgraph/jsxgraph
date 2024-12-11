jsxgraph/jsxgraph

commit 7f49c5d3926a9903c344f5c53fc720ff93f1e756

setAttribute: fix one more toLower case.

src/base/element.js
@@ -1270,7 +1270,8 @@ JXG.extend(
                     // Now, only the supplied label attributes are overwritten.
                     // Otherwise, the value of label would be {visible:false} only.
                     if (Type.isObject(value) && Type.exists(this.visProp[key])) {
-                        this.visProp[key] = Type.merge(this.visProp[key], value);
+                        // this.visProp[key] = Type.merge(this.visProp[key], value);
+                        Type.mergeAttr(this.visProp[key], value);
 
                         // First, handle the special case
                         // ticks.setAttribute({label: {anchorX: "right", ..., visible: true});

commit 506a79b8c22ab64b193d80cee0af8b3c65c88a3c

Fix regression: setAttribute
Introduced in 7f49c5d3926a9903c344f5c53fc720ff93f1e756:
In setAttribute we switched from merge to mergeAttr to avoid
one more case with upper case letters in attributes.

src/base/element.js
@@ -1271,6 +1271,12 @@ JXG.extend(
                     // Otherwise, the value of label would be {visible:false} only.
                     if (Type.isObject(value) && Type.exists(this.visProp[key])) {
                         // this.visProp[key] = Type.merge(this.visProp[key], value);
+                        if (!Type.isObject(this.visProp[key]) && Type.isObject(value)) {
+                            // Handle cases like key=firstarrow and
+                            // firstarrow==false and value = { type:1 }.
+                            // That is a primitive type is replaced by an object.
+                            this.visProp[key] = {};
+                        }
                         Type.mergeAttr(this.visProp[key], value);
 
                         // First, handle the special case

src/utils/type.js
@@ -1287,7 +1287,6 @@ JXG.extend(
             for (e in special) {
                 if (special.hasOwnProperty(e)) {
                     e2 = (toLower) ? e.toLowerCase(): e;
-
                     // Key already exists, but not in lower case
                     if (e2 !== e && attr.hasOwnProperty(e)) {
                         if (attr.hasOwnProperty(e2)) {