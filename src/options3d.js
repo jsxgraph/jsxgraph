2bc42d8d426432991ff70ffd53e3980fc32beb21
alfred <alfred.wassermann@uni-bayreuth.de>
2025-01-15 18:07:26 +0100

    Bug fix: API docs for keyboard control of az, el, bank

--- src/options3d.js
+++ src/options3d.js
@@ -663,7 +663,7 @@ JXG.extend(Options, {
          *      </ul>
          *  <li><tt>keyboard</tt> sub-attributes:
          *      <ul>
-         *          <li><tt>enabled</tt>: Boolean that specifies whether the keyboard (arrow keys) can be used to navigate the board.
+         *          <li><tt>enabled</tt>: Boolean that specifies whether the keyboard (left/right arrow keys) can be used to navigate the board.
          *          <li><tt>step</tt>: Size of the step per keystroke.
          *          <li><tt>key</tt>: Should an additional key be pressed? (<tt>'none'</tt>, <tt>'shift'</tt> or <tt>'ctrl'</tt>)
          *      </ul>
@@ -764,7 +764,7 @@ JXG.extend(Options, {
          *      </ul>
          *  <li><tt>keyboard</tt> sub-attributes:
          *      <ul>
-         *          <li><tt>enabled</tt>: Boolean that specifies whether the keyboard (arrow keys) can be used to navigate the board.
+         *          <li><tt>enabled</tt>: Boolean that specifies whether the keyboard (up/down arrow keys) can be used to navigate the board.
          *          <li><tt>step</tt>: Size of the step per keystroke.
          *          <li><tt>key</tt>: Should an additional key be pressed? (<tt>'none'</tt>, <tt>'shift'</tt> or <tt>'ctrl'</tt>)
          *      </ul>
@@ -864,7 +864,7 @@ JXG.extend(Options, {
          *      </ul>
          *  <li><tt>keyboard</tt> sub-attributes:
          *      <ul>
-         *          <li><tt>enabled</tt>: Boolean that specifies whether the keyboard (arrow keys) can be used to navigate the board.
+         *          <li><tt>enabled</tt>: Boolean that specifies whether the keyboard ('<', '>' keys) can be used to navigate the board.
          *          <li><tt>step</tt>: Size of the step per keystroke.
          *          <li><tt>key</tt>: Should an additional key be pressed? (<tt>'none'</tt>, <tt>'shift'</tt> or <tt>'ctrl'</tt>)
          *      </ul>
