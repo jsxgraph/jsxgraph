0f22be1145f6e82bcd528bd95bd7b358294a90b6
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2025-03-06 18:59:17 +0100

    Work on jsdoc3Z

---
+++ jsdoc3/nix.js
@@ -0,0 +1,35 @@
+// /**
+//  * @class 
+//  * 
+//  * @name nix2
+//  * @type Object
+//  * @description Bla, bla
+//  * 
+//  */
+// /**
+//  * Function nix variant 1
+//  * @param {number} p1
+//  * @param {number} p2
+//  * @param {object} [opts] options
+//  *  @param {boolean} [opts.opt1] option 1
+//  *  @param {number} [opts.opt2] option 2
+//  */
+/**
+ * Function nix variant 2
+ * 
+ * @param {string} p1 This is a long description about this parameter that may cover several lines of text and even MathJax
+ * @param {string} p2
+ * @param {object} [opts] options
+ *  @param {boolean} [opts.opt1] option 1
+ *  @param {number} [opts.opt2] option 2
+ * @param {*} alternative
+* @param {number} p1
+* @param {number} p2
+* @param {object} [opts] options
+*  @param {boolean} [opts.opt1] option 1
+*  @param {number} [opts.opt2] option 2
+*/
+const nix = function(p1, p2, opts) {
+    return 0;
+}
+
