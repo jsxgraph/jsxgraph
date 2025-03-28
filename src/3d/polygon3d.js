e2e565e34fc6b5c6cf5973b3ddb888d8da6646c6
alfred <alfred.wassermann@uni-bayreuth.de>
2025-03-28 11:29:22 +0100

    Add comment

--- src/3d/polygon3d.js
+++ src/3d/polygon3d.js
@@ -165,7 +165,9 @@ JXG.createPolygon3D = function (board, parents, attributes) {
             }
             points.push(board.create('point3d', [obj.vertices[i], parents[2]], attr_points));
         }
-    } else if (Type.isArray(parents[1]) && parents[1].every((x) => Type.isPoint3D(x))) {  // array of points
+    } else if (Type.isArray(parents[1]) && parents[1].every((x) => Type.isPoint3D(x))) {
+        // array of points [A, B, C]
+        // TODO mixing points and coords arrays
         points = parents[1];
     } else {
         points = Type.providePoints3D(view, parents.slice(1), attributes, 'polygon3d', ['vertices']);
