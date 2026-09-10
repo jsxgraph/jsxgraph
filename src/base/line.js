a427147e812032185f09600f35bb09178a06bac7
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2026-09-10 09:17:42 +0200

    jsdoc: again @typedef

--- src/base/line.js
+++ src/base/line.js
@@ -1181,17 +1181,24 @@ JXG.extend(
  * - coordinate array `[z, x, y]` with homogeneous coordinates.
  * In this case, `z` is 0 for infinite points, non-zero otherwise.
  *   
- * @callback FunctionPoint
+ * @callback PointFunction
+ */
+/**
+ * An array of length 2 or 3.
+ * - coordinate array `[x, y]`, so called affine coordinates
+ * - coordinate array `[z, x, y]` with homogeneous coordinates.
+ *
+ * @typedef {array} Coordinates2D
  */
 /**
  * A point, coordinates, or functions returning point or coordinates.
  *
- * @typedef {(Point | JXG.Point | number[] | FunctionPoint | function():number[])} PointLike
+ * @typedef {(Point | Coordinates2D | PointFunction)} PointType
  */
 /**
  * A line type.
  *
- * @typedef {(Line|number[]|JXG.Line)} LineLike
+ * @typedef {(Line|number[]|JXG.Line)} LineType
  */
 
 /**
@@ -1210,7 +1217,7 @@ JXG.extend(
  * It is possible to provide a function returning an array or a point, instead of providing an array or a point.
  * @param {JXG.Point | number[] | function():JXG.Point | function():number[]} point1 First point
  * @param {JXG.Point | number[] | function():JXG.Point | function():number[]} point2 Second point
- * @param {PointLike} point1 First {@link PointLike} object
+ * @param {PointLike} point1 First {@link PointType} object
  *
  * @signature board.create('line', [a, b, c])
  * Create a line from homogeneous coordinates.
